baf2f6c4e47dfcd158d9aa58818d18ccb867b6d2
alfred.stappenbeck <a@b>
2018-06-20 10:22:53 -0800

    Adding more of the progress/completion values. Not all of them though, maybe we can discuss if this pattern works for the short term also this sets an example for extending.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/api/": change = 529775]

--- main/src/main/java/com/theplatform/dfh/cp/api/progress/JobProgress.java
+++ main/src/main/java/com/theplatform/dfh/cp/api/progress/JobProgress.java
@@ -3,6 +3,7 @@ package com.theplatform.dfh.cp.api.progress;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.net.URI;
+import java.util.Arrays;
 import java.util.Date;
 
 public class JobProgress
@@ -156,4 +157,83 @@ public class JobProgress
         setPercentComplete(100L);
         setStatus(JobStatus.RUN_COMPLETE);
     }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        JobProgress that = (JobProgress) o;
+
+        if (isHalted() != that.isHalted())
+            return false;
+        if (getId() != null
+            ? !getId().equals(that.getId())
+            : that.getId() != null)
+            return false;
+        if (getJobId() != null
+            ? !getJobId().equals(that.getJobId())
+            : that.getJobId() != null)
+            return false;
+        if (getStatus() != that.getStatus())
+            return false;
+        if (getAdded() != null
+            ? !getAdded().equals(that.getAdded())
+            : that.getAdded() != null)
+            return false;
+        if (getStarted() != null
+            ? !getStarted().equals(that.getStarted())
+            : that.getStarted() != null)
+            return false;
+        if (getCompleted() != null
+            ? !getCompleted().equals(that.getCompleted())
+            : that.getCompleted() != null)
+            return false;
+        if (getPercentComplete() != null
+            ? !getPercentComplete().equals(that.getPercentComplete())
+            : that.getPercentComplete() != null)
+            return false;
+        // Probably incorrect - comparing Object[] arrays with Arrays.equals
+        if (!Arrays.equals(getOperationProgresses(), that.getOperationProgresses()))
+            return false;
+        return getConclusionStatus() == that.getConclusionStatus();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = getId() != null
+            ? getId().hashCode()
+            : 0;
+        result = 31 * result + (getJobId() != null
+            ? getJobId().hashCode()
+            : 0);
+        result = 31 * result + (getStatus() != null
+            ? getStatus().hashCode()
+            : 0);
+        result = 31 * result + (getAdded() != null
+            ? getAdded().hashCode()
+            : 0);
+        result = 31 * result + (getStarted() != null
+            ? getStarted().hashCode()
+            : 0);
+        result = 31 * result + (getCompleted() != null
+            ? getCompleted().hashCode()
+            : 0);
+        result = 31 * result + (getPercentComplete() != null
+            ? getPercentComplete().hashCode()
+            : 0);
+        result = 31 * result + Arrays.hashCode(getOperationProgresses());
+        result = 31 * result + (isHalted()
+            ? 1
+            : 0);
+        result = 31 * result + (getConclusionStatus() != null
+            ? getConclusionStatus().hashCode()
+            : 0);
+        return result;
+    }
+
 }
