ceb6e0bd9e236689019f70b99a8b2a3e303c261a
Tim Stair <tim.stair@theplatform.com>
2018-07-25 12:53:55 -0800

    [CP] Handler sample, initial exploration.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/handler/sample/api/main/": change = 530811]

---
+++ src/main/java/com/theplatform/dfh/cp/handler/sample/api/SampleInput.java
@@ -0,0 +1,16 @@
+package com.theplatform.dfh.cp.handler.sample.api;
+
+public class SampleInput
+{
+    private String sampleField;
+
+    public String getSampleField()
+    {
+        return sampleField;
+    }
+
+    public void setSampleField(String sampleField)
+    {
+        this.sampleField = sampleField;
+    }
+}
