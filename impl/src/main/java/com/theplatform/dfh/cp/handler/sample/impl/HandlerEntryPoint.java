d2106798b4c733fb4b11409a80499e57fb88fea3
Tim Stair <tim.stair@theplatform.com>
2018-08-03 15:00:49 -0800

    [CP] Updated sample handler.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/handler/sample/handler/main/": change = 531037]

--- impl/src/main/java/com/theplatform/dfh/cp/handler/sample/impl/HandlerEntryPoint.java
+++ impl/src/main/java/com/theplatform/dfh/cp/handler/sample/impl/HandlerEntryPoint.java
@@ -1,31 +1,42 @@
 package com.theplatform.dfh.cp.handler.sample.impl;
 
-import com.theplatform.dfh.cp.handler.field.api.HandlerField;
-import com.theplatform.dfh.cp.handler.field.retriever.environment.EnvironmentFieldRetriever;
-import com.theplatform.dfh.cp.handler.field.translator.api.FieldTranslator;
-import com.theplatform.dfh.cp.handler.field.translator.json.JsonFieldTranslator;
+import com.theplatform.dfh.cp.handler.field.retriever.DefaultLaunchDataWrapper;
+import com.theplatform.dfh.cp.handler.field.retriever.LaunchDataWrapper;
 import com.theplatform.dfh.cp.handler.reporter.api.ReporterSet;
 import com.theplatform.dfh.cp.handler.reporter.kubernetes.KubernetesReporterSet;
 import com.theplatform.dfh.cp.handler.sample.api.SampleInput;
+import com.theplatform.dfh.cp.jsonhelper.JsonHelper;
 
 public class HandlerEntryPoint
 {
     private ReporterSet reporterSet = new KubernetesReporterSet();
-    private JsonFieldTranslator fieldTranslator;
+    private LaunchDataWrapper launchDataWrapper;
+    private JsonHelper jsonHelper;
 
-    public HandlerEntryPoint(JsonFieldTranslator fieldTranslator)
+    public HandlerEntryPoint(String[] args)
     {
-        this.fieldTranslator = fieldTranslator;
+        launchDataWrapper = new DefaultLaunchDataWrapper(args);
+        jsonHelper = new JsonHelper();
     }
 
+    /**
+     * Local/IDEA non-docker execution prerequisites:
+     * - debugging/running with a local-only build use these args (will definitely need to adjust the payload.json accordingly):
+     * -launcherType local -propFile ./handler/main/package/local/config/external.properties -payloadFile ./handler/main/package/local/payload.json
+     *
+     * @param args command line args
+     */
     public static void main(String[] args)
     {
-        new HandlerEntryPoint(new JsonFieldTranslator(new EnvironmentFieldRetriever())).execute();
+        // just for convenience...
+        System.out.println(System.getProperty("user.dir"));
+
+        new HandlerEntryPoint(args).execute();
     }
 
     public void execute()
     {
-        SampleInput sampleInput = fieldTranslator.getObject(HandlerField.PAYLOAD, SampleInput.class);
+        SampleInput sampleInput = jsonHelper.getObjectFromString(launchDataWrapper.getPayload(), SampleInput.class);
 
         reporterSet.reportProgress(sampleInput);
         reporterSet.reportProgress(sampleInput);
@@ -35,5 +46,15 @@ public class HandlerEntryPoint
     public void setReporterSet(ReporterSet reporterSet)
     {
         this.reporterSet = reporterSet;
+    }
+
+    public void setLaunchDataWrapper(LaunchDataWrapper launchDataWrapper)
+    {
+        this.launchDataWrapper = launchDataWrapper;
+    }
+
+    public void setJsonHelper(JsonHelper jsonHelper)
+    {
+        this.jsonHelper = jsonHelper;
     }
 }
