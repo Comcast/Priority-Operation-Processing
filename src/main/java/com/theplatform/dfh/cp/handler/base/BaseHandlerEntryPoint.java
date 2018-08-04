4b70c93af1d6562b74e3b5dff78fd1efc13ac2bf
Tim Stair <tim.stair@theplatform.com>
2018-08-04 15:24:59 -0800

    [CP] Move the handler base under modules and eliminate the parent pom.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/handler/modules/base/main/": change = 531056]

---
+++ src/main/java/com/theplatform/dfh/cp/handler/base/BaseHandlerEntryPoint.java
@@ -0,0 +1,7 @@
+package com.theplatform.dfh.cp.handler.base;
+
+// TODO: determine if we need this...
+public abstract class BaseHandlerEntryPoint
+{
+
+}
---
+++ src/main/java/com/theplatform/dfh/cp/handler/base/perform/RetryableExecutor.java
@@ -0,0 +1,109 @@
+package com.theplatform.dfh.cp.handler.base.perform;
+
+import net.jodah.failsafe.Failsafe;
+import net.jodah.failsafe.RetryPolicy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Performs operations with a retry policy around exception handling
+ * @param <T> The type of object returned by the operation
+ */
+public class RetryableExecutor<T> implements Executor<T>
+{
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Executor<T> executor;
+    private final int maxRetries;
+    private int currentRetry = 0;
+    private List<Class<? extends Throwable>> nonRetryableExceptions;
+    private List<Class<? extends Throwable>> retryableExceptions;
+
+    /**
+     * Constructor that defaults to retrying on RuntimExceptions
+     * @param executor The operation performer to call
+     * @param maxRetries The maximum number of retries to allow (0+)
+     */
+    public RetryableExecutor(
+        Executor<T> executor,
+        int maxRetries)
+    {
+        this(executor, maxRetries, Collections.singletonList(RuntimeException.class));
+    }
+    /**
+     * Constructor
+     * @param executor The operation performer to call
+     * @param maxRetries The maximum number of retries to allow (0+)
+     * @param retryableExceptions The retryable Exceptions
+     */
+    public RetryableExecutor(
+        Executor<T> executor,
+        int maxRetries,
+        List<Class<? extends Throwable>> retryableExceptions)
+    {
+        this.executor = executor;
+        this.maxRetries = maxRetries;
+        this.retryableExceptions = filterOutNulls(retryableExceptions);
+    }
+
+    /**
+     * Constructor
+     * @param operationPerformer The operation performer to call
+     * @param maxRetries The maximum number of retries to allow (0+)
+     * @param retryableExceptions The retryable Exceptions
+     * @param nonRetryableExceptions The non-retryable Exceptions
+     */
+    public RetryableExecutor(
+        Executor<T> operationPerformer,
+        int maxRetries,
+        List<Class<? extends Throwable>> retryableExceptions,
+        List<Class<? extends Throwable>> nonRetryableExceptions)
+    {
+        this(operationPerformer, maxRetries, retryableExceptions);
+        this.nonRetryableExceptions = filterOutNulls(nonRetryableExceptions);
+    }
+
+    private static List<Class<? extends Throwable>> filterOutNulls(List<Class<? extends Throwable>> list)
+    {
+        return Optional.ofNullable(list).orElse(new ArrayList<>()).stream().filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
+    /**
+     * Performs the operation with a retry policy
+     * @return The result of the operation.
+     */
+    @Override
+    public T execute()
+    {
+        logger.debug("Initializing retry");
+        RetryPolicy policy = new RetryPolicy();
+
+        if (retryableExceptions != null && retryableExceptions.size() > 0)
+        {
+            policy = policy.retryOn(retryableExceptions);
+        }
+
+        if (nonRetryableExceptions != null && nonRetryableExceptions.size() > 0)
+        {
+            policy = policy.abortOn(nonRetryableExceptions);
+        }
+
+        policy = policy.withMaxRetries(maxRetries);
+
+        return Failsafe
+            .with(policy)
+            .onRetry(exception ->
+            {
+                currentRetry++;
+                logger.error("Retrying Operation {} :", currentRetry, exception);
+            })
+            .get((() ->
+            {
+                logger.debug("Running Operation - Attempt: {}", currentRetry);
+                return executor.execute();
+            }));
+    }
+
+}
