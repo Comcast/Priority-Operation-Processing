551055ef6325495e465965d716e99fbf9f1d7fa4
kimberly.todd <a@b>
2018-08-27 14:44:05 -0800

    [CP] wait for pod to finish before looking at logs
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/modules/kubeclient/": change = 531678]

--- test/main/src/test/java/com/theplatform/dfh/cp/modules/kube/fabric8/test/PodNameTest.java
+++ test/main/src/test/java/com/theplatform/dfh/cp/modules/kube/fabric8/test/PodNameTest.java
@@ -5,6 +5,8 @@ import com.theplatform.dfh.cp.modules.kube.client.config.PodConfig;
 import com.theplatform.dfh.cp.modules.kube.fabric8.client.Fabric8Helper;
 import com.theplatform.dfh.cp.modules.kube.fabric8.client.PodPushClient;
 import com.theplatform.dfh.cp.modules.kube.fabric8.client.factory.PodPushClientFactoryImpl;
+import com.theplatform.dfh.cp.modules.kube.fabric8.client.follower.PodFollower;
+import com.theplatform.dfh.cp.modules.kube.fabric8.client.follower.PodFollowerImpl;
 import com.theplatform.dfh.cp.modules.kube.fabric8.client.modulator.HiLowCpuRequestModulator;
 import io.fabric8.kubernetes.api.model.DoneablePod;
 import io.fabric8.kubernetes.api.model.EnvVar;
@@ -30,20 +32,21 @@ public class PodNameTest extends KubeClientTestBase
     @Test
     public void testPodName_ExternalId_CanBeSeen() throws Exception
     {
-        PodPushClient client = new PodPushClientFactoryImpl().getClient(kubeConfig);
 
         PodConfig podConfig = new PodConfig();
         podConfig.setImageName("ubuntu:14.04");
         podConfig.setArguments(new String[] { "printenv" });
         podConfig.setNamePrefix("testpodname");
+        podConfig.setReapCompletedPods(false);
 
         ExecutionConfig executionConfig = new ExecutionConfig(podConfig.getNamePrefix());
         executionConfig.addEnvVar("LOG_LEVEL", "DEBUG");
         executionConfig.setCpuRequestModulator(new HiLowCpuRequestModulator());
 
-        client.startWithoutWatcher(podConfig, executionConfig);
+        PodFollower podFollower = new PodFollowerImpl(kubeConfig, podConfig, executionConfig);
+        podFollower.startAndFollowPod(podFollower.getDefaultLogLineObserver(executionConfig));
 
-        PodResource<Pod, DoneablePod> p = client.getFabric8Client()
+        PodResource<Pod, DoneablePod> p = podFollower.getPodPushClient().getFabric8Client()
             .inNamespace(kubeConfig.getNameSpace()).pods()
             .withName(executionConfig.getName());
         Thread.sleep(10000);
@@ -72,7 +75,7 @@ public class PodNameTest extends KubeClientTestBase
         }
         finally
         {
-            client.deletePod(executionConfig.getName());
+            podFollower.getPodPushClient().deletePod(executionConfig.getName());
         }
     }
 }
