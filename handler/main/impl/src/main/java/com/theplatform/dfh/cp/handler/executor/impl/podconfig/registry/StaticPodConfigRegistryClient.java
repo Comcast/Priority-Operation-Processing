5189fed1df5c3637e20acc6cf182d66e253da89b
Tim Stair <tim.stair@theplatform.com>
2018-08-29 14:54:05 -0800

    [CP] ConfigMaps and updated registry for local executor.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/handler/executor/": change = 531747]

--- handler/main/impl/src/main/java/com/theplatform/dfh/cp/handler/executor/impl/podconfig/registry/StaticPodConfigRegistryClient.java
+++ handler/main/impl/src/main/java/com/theplatform/dfh/cp/handler/executor/impl/podconfig/registry/StaticPodConfigRegistryClient.java
@@ -1,6 +1,7 @@
 package com.theplatform.dfh.cp.handler.executor.impl.podconfig.registry;
 
 import com.theplatform.dfh.cp.modules.kube.client.config.ConfigMapDetails;
+import com.theplatform.dfh.cp.modules.kube.client.config.NfsDetails;
 import com.theplatform.dfh.cp.modules.kube.client.config.PodConfig;
 import com.theplatform.dfh.cp.podconfig.registry.client.api.PodConfigRegistryClient;
 
@@ -25,7 +26,14 @@ public class StaticPodConfigRegistryClient implements PodConfigRegistryClient
                 .setImageName("docker-lab.repo.theplatform.com/fhsamp:1.0.0")
                 .setNamePrefix("dfh-samp")
                 .setEndOfLogIdentifier("SampleComplete")
+                .setConfigMapDetails(new ConfigMapDetails()
+                    .setConfigMapName("lab-main-t-aor-fhsamp-t01")
+                    .setMapKey("external-properties")
+                    .setMapPath("external.properties")
+                    .setVolumeName("config-volume")
+                    .setVolumeMountPath("/config"))
         );
+
         podConfigMap.put("encode",
             new PodConfig()
                 .setServiceAccountName("ffmpeg-service")
@@ -43,7 +51,34 @@ public class StaticPodConfigRegistryClient implements PodConfigRegistryClient
                     .setMapKey("external-properties")
                     .setMapPath("external.properties")
                     .setVolumeName("config-volume")
-                    .setVolumeMountPath("/config")));
+                    .setVolumeMountPath("/config"))
+        );
+
+        // minikube specific encode handler
+        podConfigMap.put("encodeMinikube",
+            new PodConfig()
+                .setServiceAccountName("ffmpeg-service")
+                .setMemoryRequestCount("1000m")
+                .setCpuMinRequestCount("1000m")
+                .setCpuMaxRequestCount("1000m")
+                .setPodScheduledTimeoutMs(600000L)
+                .setReapCompletedPods(true)
+                .setPullAlways(true) // for now
+                .setImageName("docker-lab.repo.theplatform.com/fheff:1.0.0")
+                .setNamePrefix("dfh-encode")
+                .setEndOfLogIdentifier("ffmpeg_handler_end")
+                .setConfigMapDetails(new ConfigMapDetails()
+                    .setConfigMapName("lab-main-t-aor-fheff-t01")
+                    .setMapKey("external-properties")
+                    .setMapPath("external.properties")
+                    .setVolumeName("config-volume")
+                    .setVolumeMountPath("/config"))
+                .setNfsDetails(new NfsDetails()
+                    .setNfsServerPath("/")
+                    .setNfsServer("192.168.99.1")
+                    .setNfsMountPaths(new String[] {"/mnt"})
+                    .setNfsReadOnly(false))
+        );
     }
 
     @Override
