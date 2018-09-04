95627a043f1e85029ca9fe2e08d6d338012911c4
kimberly.todd <a@b>
2018-09-04 12:07:27 -0800

    remove payloadFile arg from dockerRun.sh for Puller.  There is no payloadFile used
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/handler/puller/": change = 531810]

--- handler/main/package/dockerRun.sh
+++ handler/main/package/dockerRun.sh
@@ -5,7 +5,7 @@ DIR=$(dirname $0)
 
 IMAGE=fhpull:1.0.0
 #IMAGE=$'docker-lab.repo.theplatform.com/feh:1.0.1'
-ARGS="-externalLaunchType local -propFile ./handler/main/package/local/config/external.properties -payloadFile ./handler/main/package/local/payload.json"
+ARGS="-externalLaunchType local -propFile ./handler/main/package/local/config/external.properties"
 
 docker run --env-file env.list -it      \
            -v ${DIR}/local/config:/config \
