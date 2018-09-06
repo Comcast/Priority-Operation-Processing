71b43ba3ecdb823d92545f27dea1b10a3ff2d7ea
kimberly.todd <a@b>
2018-09-06 15:58:23 -0800

    [CP] move main method from PullerApp to PullerEntryPoint
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/handler/puller/": change = 531902]

--- handler/main/impl/src/main/java/com/theplatform/dfh/cp/handler/puller/impl/PullerApp.java
+++ handler/main/impl/src/main/java/com/theplatform/dfh/cp/handler/puller/impl/PullerApp.java
@@ -1,9 +1,7 @@
 package com.theplatform.dfh.cp.handler.puller.impl;
 
-import com.theplatform.dfh.cp.handler.field.retriever.api.FieldRetriever;
 import com.theplatform.dfh.cp.handler.puller.impl.config.PullerConfig;
 import com.theplatform.dfh.cp.handler.puller.impl.healthcheck.AliveHealthCheck;
-import com.theplatform.dfh.cp.handler.puller.impl.retriever.PullerArgumentProvider;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Environment;
 import org.slf4j.Logger;
@@ -28,27 +26,5 @@ public class PullerApp extends Application<PullerConfig>
         environment.healthChecks().register("basic-health", new AliveHealthCheck(pullerExecution.getExecutionContext()));
 
         pullerExecution.start();
-    }
-
-
-    /**
-     * Debugging/running with a local-only build use these args:
-     * -launchType local -externalLaunchType local -propFile ./handler/main/package/local/config/external.properties -confPath ./handler/main/package/local/config/conf.yaml
-     *
-     * Debugging running local with minikube for pod launches
-     * -launchType local -externalLaunchType kubernetes -propFile ./handler/main/package/local/config/external-minikube.properties
-     *
-     * Debugging/running local with kubernetes for pod launches: (requires environment vars for k8s auth)
-     * -launchType local -externalLaunchType kubernetes -propFile ./handler/main/package/local/config/external.properties
-     *
-     * @param args command line args
-     */
-    public static void main( String[] args ) throws Exception
-    {
-        PullerEntryPoint pullerEntryPoint = new PullerEntryPoint(args);
-        FieldRetriever argumentRetriever = pullerEntryPoint.getLaunchDataWrapper().getArgumentRetriever();
-        String confPath = argumentRetriever.getField(PullerArgumentProvider.CONF_PATH, "/app/config/conf.yaml");
-        String[] args2 = new String[] {"server", confPath};
-        new PullerApp(pullerEntryPoint).run(args2);
     }
 }
--- handler/main/impl/src/main/java/com/theplatform/dfh/cp/handler/puller/impl/PullerEntryPoint.java
+++ handler/main/impl/src/main/java/com/theplatform/dfh/cp/handler/puller/impl/PullerEntryPoint.java
@@ -2,6 +2,7 @@ package com.theplatform.dfh.cp.handler.puller.impl;
 
 import com.theplatform.dfh.cp.handler.base.BaseHandlerEntryPoint;
 import com.theplatform.dfh.cp.handler.base.context.BaseOperationContextFactory;
+import com.theplatform.dfh.cp.handler.field.retriever.api.FieldRetriever;
 import com.theplatform.dfh.cp.handler.field.retriever.argument.ArgumentRetriever;
 import com.theplatform.dfh.cp.handler.puller.impl.client.agenda.AgendaClient;
 import com.theplatform.dfh.cp.handler.puller.impl.client.agenda.DefaultAgendaClientFactory;
@@ -42,5 +43,26 @@ public class PullerEntryPoint extends BaseHandlerEntryPoint<PullerContext, Pulle
     protected PullerProcessor createHandlerProcessor(LaunchDataWrapper launchDataWrapper, PullerContext handlerContext)
     {
         return new PullerProcessor(launchDataWrapper, handlerContext, agendaClient);
+    }
+
+    /**
+     * Debugging/running with a local-only build use these args:
+     * -launchType local -externalLaunchType local -propFile ./handler/main/package/local/config/external.properties -confPath ./handler/main/package/local/config/conf.yaml
+     *
+     * Debugging running local with minikube for pod launches
+     * -launchType local -externalLaunchType kubernetes -propFile ./handler/main/package/local/config/external-minikube.properties -confPath ./handler/main/package/local/config/conf.yaml
+     *
+     * Debugging/running local with kubernetes for pod launches: (requires environment vars for k8s auth)
+     * -launchType local -externalLaunchType kubernetes -propFile ./handler/main/package/local/config/external.properties -confPath ./handler/main/package/local/config/conf.yaml
+     *
+     * @param args command line args
+     */
+    public static void main( String[] args ) throws Exception
+    {
+        PullerEntryPoint pullerEntryPoint = new PullerEntryPoint(args);
+        FieldRetriever argumentRetriever = pullerEntryPoint.getLaunchDataWrapper().getArgumentRetriever();
+        String confPath = argumentRetriever.getField(PullerArgumentProvider.CONF_PATH, "/app/config/conf.yaml");
+        String[] args2 = new String[] {"server", confPath};
+        new PullerApp(pullerEntryPoint).run(args2);
     }
 }
