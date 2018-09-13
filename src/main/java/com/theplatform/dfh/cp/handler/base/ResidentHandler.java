d601d47034fed28b60ed2ff96cd4a575615926e0
Tim Stair <tim.stair@theplatform.com>
2018-09-13 07:43:10 -0800

    [CP] Update the ResidentHandler interface to pass a launch data wrapper and a reporter.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/handler/modules/base/main/": change = 532018]

--- src/main/java/com/theplatform/dfh/cp/handler/base/ResidentHandler.java
+++ src/main/java/com/theplatform/dfh/cp/handler/base/ResidentHandler.java
@@ -1,9 +1,12 @@
 package com.theplatform.dfh.cp.handler.base;
 
+import com.theplatform.dfh.cp.handler.field.retriever.LaunchDataWrapper;
+import com.theplatform.dfh.cp.handler.reporter.api.Reporter;
+
 /**
  * Represents a handler that is executed directly as a class (no external launch to execute)
  */
 public interface ResidentHandler
 {
-    String execute(String payload);
+    String execute(String payload, LaunchDataWrapper launchDataWrapper, Reporter reporter);
 }
