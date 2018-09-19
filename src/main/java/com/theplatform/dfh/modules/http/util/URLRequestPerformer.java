95fe67719b13b9eff701c2d9a414a2ca54542d56
Tim Stair <tim.stair@theplatform.com>
2018-09-19 13:27:00 -0800

    [CP] Moved Http functionality into its own module (away from scheduling).
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/modules/Http/util/main/": change = 532218]

---
+++ src/main/java/com/theplatform/dfh/modules/http/util/URLRequestPerformer.java
@@ -0,0 +1,49 @@
+package com.theplatform.dfh.modules.http.util;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+public class URLRequestPerformer
+{
+    /**
+     * Makes a request to the specified HttpURLConnection
+     * @param connection The connection to operate with
+     * @param data The data to write in the POST of the request (may be null)
+     * @return The result of the request
+     * @throws IOException Thrown if there are any errors while performing the request
+     */
+    public String performURLRequest(HttpURLConnection connection, byte[] data) throws IOException
+    {
+        // only write data if there is any
+        if(data != null)
+        {
+            connection.setDoOutput(true);
+            OutputStream output = connection.getOutputStream();
+            output.write(data);
+            output.close();
+        }
+
+        InputStream is = connection.getInputStream();
+
+        String result = convert(is, StandardCharsets.UTF_8);
+        is.close();
+        return result;
+    }
+
+    /**
+     * Converts an input stream to a String based on the specified charset
+     * @param inputStream The input stream to read from
+     * @param charset The charset to read using
+     * @return The converted String
+     * @throws IOException
+     */
+    protected String convert(InputStream inputStream, Charset charset) throws IOException {
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, charset))) {
+            return br.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+    }
+}
