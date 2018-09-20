963f9f52ce26d7211c5b3c50e8fe01c13bbef3c9
Tim Stair <tim.stair@theplatform.com>
2018-09-20 11:30:30 -0800

    [DFH] Added compression module (zlib implementation)
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/modules/Compression/main/": change = 532242]

---
+++ src/test/java/com/theplatform/dfh/compression/zlib/ZlibUtilTest.java
@@ -0,0 +1,26 @@
+package com.theplatform.dfh.compression.zlib;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ZlibUtilTest
+{
+    private ZlibUtil zlibUtil;
+
+    @BeforeMethod
+    public void setup()
+    {
+        zlibUtil = new ZlibUtil();
+    }
+
+    @Test
+    public void testInflateDeflate() throws Exception
+    {
+        String original = "{\"foo\":\"bar\"}";
+        byte[] in = original.getBytes("UTF-8");
+        byte[] b = zlibUtil.deflateMe(in);
+        String json = zlibUtil.inflateMe(b);
+        Assert.assertEquals(json, original);
+    }
+}
