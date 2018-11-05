044261e9eecf3a0405b3c0550d439d750cca4935
Tim Stair <tim.stair@theplatform.com>
2018-11-05 11:14:32 -0800

    [CP] Added cid as a general param key and handler field.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/api/": change = 533186]

--- main/src/main/java/com/theplatform/dfh/cp/api/params/GeneralParamKey.java
+++ main/src/main/java/com/theplatform/dfh/cp/api/params/GeneralParamKey.java
@@ -2,7 +2,7 @@ package com.theplatform.dfh.cp.api.params;
 
 public enum GeneralParamKey implements ParamKey
 {
-    progressId, execProgressId, agendaId, dependsOn;
+    progressId, execProgressId, agendaId, dependsOn, cid;
 
     public String getKey()
     {
