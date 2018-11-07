66fec3cd2d6af107e001b7a3b93b46cc06223856
Tiffany Hallisey <Tiffany.Hallisey@theplatform.com>
2018-11-06 17:00:23 -0800

    babs -- Added identity call prior to our call, but getting a bad content type exception from identity.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/web/": change = 533232]

--- src/main/resources/login.js
+++ src/main/resources/login.js
@@ -9,41 +9,55 @@ function authorizeWithLambda(e) {
         alert ("Please enter your password");
         return;
     }
+    if ($("#id_value").val()=="") {
+        alert ("Please enter your agenda progress ID");
+        return;
+    }
     var username = $("#mpx_username").val();
     var  password = $("#mpx_password").val();
-
-    var data = {
-        username : username,
-        password : password
-    };
+    var  id_value = $("#id_value").val();
 
     $.ajax({
         type: "POST",
-        url : "https://atcfap5cvk.execute-api.us-west-2.amazonaws.com/dev/dfh/idm/progress",
+        url: "https://identity.auth.test.corp.theplatform.com/idm/web/Authentication/signIn?form=json&schema=1.0",
         dataType: "json",
         crossDomain: "true",
-        contentType: "application/json; charset=utf-8",
 
         headers: {
             "Authorization": "Basic " + btoa(username + ":" + password),
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json"
+            "Accept": "*/*",
+            "Content-Type": "application/json; charset=utf-8"
         },
-        //"Access-Control-Allow-Credentials":"true",
-       // "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-       // "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method,
-        // Access-Control-Request-Headers"
+        success: function (data) {
+            $.ajax({
+                type: "GET",
+                url: "https://3io93ms3z8.execute-api.us-west-2.amazonaws.com/dev/dfh/idm/progress/agenda/" +id_value,
+                dataType: "json",
+                crossDomain: "true",
+                contentType: "application/json",
 
-       // data: JSON.stringify(data),
+                headers: {
+                    "Authorization": "Basic " + btoa(data),
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json"
+                },
+                data: JSON.stringify(data),
 
-        success: function () {
-            // clear form and show a success message
-            alert("Successfull");
-            document.getElementById("contact-form").reset();
-            location.reload();
+                success: function () {
+                    // clear form and show a success message
+                    alert("Successfull");
+                    document.getElementById("contact-form").reset();
+                    location.reload();
+                },
+                error: function () {
+                    // show an error message
+                    alert("UnSuccessfull");
+                }
+            });
         },
         error: function () {
             // show an error message
             alert("UnSuccessfull");
-        }});
+        }
+    });
 }
