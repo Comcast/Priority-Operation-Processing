5ddb033176157e82b9ba649f904d639884109585
Tim Stair <tim.stair@theplatform.com>
2018-11-09 10:43:54 -0800

    [CP] Added externalId GeneralParamKey for optional use with Agendas.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/api/": change = 533326]

--- main/src/main/java/com/theplatform/dfh/cp/api/params/GeneralParamKey.java
+++ main/src/main/java/com/theplatform/dfh/cp/api/params/GeneralParamKey.java
@@ -2,7 +2,7 @@ package com.theplatform.dfh.cp.api.params;
 
 public enum GeneralParamKey implements ParamKey
 {
-    progressId, execProgressId, agendaId, dependsOn, cid;
+    progressId, execProgressId, agendaId, dependsOn, cid, externalId;
 
     public String getKey()
     {
