0bfcd43f9f873cd815f8c4c16fe57ee53502b305
Tim Stair <tim.stair@theplatform.com>
2018-11-14 09:00:09 -0800

    [CP] Scheduling API
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/api/": change = 533411]

---
+++ scheduling/main/src/main/java/com/theplatform/dfh/cp/scheduling/api/ReadyAgenda.java
@@ -0,0 +1,11 @@
+package com.theplatform.dfh.cp.scheduling.api;
+
+import java.util.Date;
+
+public class ReadyAgenda
+{
+    String insightId;
+    String agendaId;
+    String customerId;
+    Date added;
+}
