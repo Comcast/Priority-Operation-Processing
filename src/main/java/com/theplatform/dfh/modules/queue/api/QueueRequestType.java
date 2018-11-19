82fdf5c582dfd7f93bb119f9843d909072771615
Tim Stair <tim.stair@theplatform.com>
2018-11-19 13:28:34 -0800

    [DFH] Adding Queue API.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/modules/Queue/api/main/": change = 533541]

---
+++ src/main/java/com/theplatform/dfh/modules/queue/api/QueueRequestType.java
@@ -0,0 +1,12 @@
+package com.theplatform.dfh.modules.queue.api;
+
+/**
+ * The type of request to perform on the queue
+ */
+public enum QueueRequestType
+{
+    add,
+    poll,
+    peek,
+    size
+}
---
+++ src/main/java/com/theplatform/dfh/modules/queue/api/access/ItemQueueWriter.java
@@ -0,0 +1,29 @@
+package com.theplatform.dfh.modules.queue.api.access;
+
+import com.theplatform.dfh.modules.queue.api.ItemQueue;
+import com.theplatform.dfh.modules.queue.api.QueueResult;
+
+import java.util.Collection;
+
+/**
+ * Provides write access to an underlying ItemQueue
+ */
+public class ItemQueueWriter<T>
+{
+    private final ItemQueue<T> itemQueue;
+
+    public ItemQueueWriter(ItemQueue<T> itemQueue)
+    {
+        this.itemQueue = itemQueue;
+    }
+
+    public QueueResult<T> add(T item)
+    {
+        return itemQueue.add(item);
+    }
+
+    public QueueResult<T> add(Collection<T> items)
+    {
+        return itemQueue.add(items);
+    }
+}
