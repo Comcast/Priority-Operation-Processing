341fec96c54d6ecad9310929d6c0b5d294ca6eeb
Tim Stair <tim.stair@theplatform.com>
2018-11-21 14:10:23 -0800

    [CP] Moved the DynamoDBCompressedObjectPersisterFactory
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/modules/Persistence/": change = 533630]

---
+++ aws/dynamodb/main/src/main/java/com/theplatform/dfh/persistence/aws/dynamodb/DynamoDBCompressedObjectPersisterFactory.java
@@ -0,0 +1,25 @@
+package com.theplatform.dfh.persistence.aws.dynamodb;
+
+import com.theplatform.dfh.persistence.api.ObjectPersister;
+import com.theplatform.dfh.persistence.api.ObjectPersisterFactory;
+
+/**
+ * Wrapper factory for DynamoDBCompressedObjectPersister objects so we can swap in others later (and convenient unit testing)
+ */
+public class DynamoDBCompressedObjectPersisterFactory<T> implements ObjectPersisterFactory<T>
+{
+    protected String persistenceKeyFieldName;
+    protected Class persistentObjectClass;
+
+    public DynamoDBCompressedObjectPersisterFactory(String persistenceKeyFieldName, Class<T> clazz)
+    {
+        this.persistenceKeyFieldName = persistenceKeyFieldName;
+        this.persistentObjectClass = clazz;
+    }
+
+    @Override
+    public ObjectPersister getObjectPersister(String containerName)
+    {
+        return new DynamoDBCompressedObjectPersister<T>(containerName, persistenceKeyFieldName, new AWSDynamoDBFactory(), persistentObjectClass);
+    }
+}
