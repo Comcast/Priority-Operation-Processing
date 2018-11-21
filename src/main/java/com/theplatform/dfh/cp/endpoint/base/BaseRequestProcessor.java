12a392b34a2451d256d5b1262d7c28f0f35391d8
Tiffany Hallisey <Tiffany.Hallisey@theplatform.com>
2018-11-21 10:35:14 -0800

    babs -- Fission : Adding exception handling for the new persistence exception
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/modules/BaseEndpoint/main/": change = 533607]

--- src/main/java/com/theplatform/dfh/cp/endpoint/base/BaseRequestProcessor.java
+++ src/main/java/com/theplatform/dfh/cp/endpoint/base/BaseRequestProcessor.java
@@ -1,8 +1,10 @@
 package com.theplatform.dfh.cp.endpoint.base;
 
 import com.theplatform.dfh.cp.api.IdentifiedObject;
+import com.theplatform.dfh.cp.endpoint.api.BadRequestException;
 import com.theplatform.dfh.cp.endpoint.api.ObjectPersistResponse;
 import com.theplatform.dfh.persistence.api.ObjectPersister;
+import com.theplatform.dfh.persistence.api.PersistenceException;
 
 import java.util.UUID;
 
@@ -24,9 +26,16 @@ public abstract class BaseRequestProcessor<T extends IdentifiedObject>
      * @param id id of the object to get
      * @return The object, or null if not found
      */
-    public T handleGET(String id)
+    public T handleGET(String id) throws BadRequestException
     {
-        return objectPersister.retrieve(id);
+        try
+        {
+            return objectPersister.retrieve(id);
+        }
+        catch(PersistenceException e)
+        {
+            throw new BadRequestException(String.format("Unable to get object by id {}", id), e);
+        }
     }
 
     /**
@@ -34,11 +43,18 @@ public abstract class BaseRequestProcessor<T extends IdentifiedObject>
      * @param objectToPersist The object to persist
      * @return Resulting id of the persisted object
      */
-    public ObjectPersistResponse handlePOST(T objectToPersist)
+    public ObjectPersistResponse handlePOST(T objectToPersist) throws BadRequestException
     {
         String objectId = UUID.randomUUID().toString();
         objectToPersist.setId(objectId);
-        objectPersister.persist(objectId, objectToPersist);
+        try
+        {
+            objectPersister.persist(objectId, objectToPersist);
+        }
+        catch(PersistenceException e)
+        {
+            throw new BadRequestException("Unable to create object", e);
+        }
         return new ObjectPersistResponse(objectId);
     }
 
@@ -46,15 +62,32 @@ public abstract class BaseRequestProcessor<T extends IdentifiedObject>
      * Handles a PUT of an object
      * @param objectToUpdate object to persist
      */
-    public void handlePUT(T objectToUpdate)
+    public void handlePUT(T objectToUpdate) throws BadRequestException
     {
         // NOTE: the default update implementation is just a persist call
-        objectPersister.update(objectToUpdate.getId(), objectToUpdate);
+        try
+        {
+            objectPersister.update(objectToUpdate.getId(), objectToUpdate);
+        }
+        catch(PersistenceException e)
+        {
+            final String id = objectToUpdate == null ? "UNKNOWN" : objectToUpdate.getId();
+            throw new BadRequestException(String.format("Unable to update object by id {}", id), e);
+        }
     }
 
     /**
      * Handles the DELETE of an object // TODO: should this return an object?
      * @param id The id of the object to delete
      */
-    public void handleDelete(String id) { objectPersister.delete(id); }
+    public void handleDelete(String id) throws BadRequestException{
+        try
+        {
+            objectPersister.delete(id);
+        }
+        catch(PersistenceException e)
+        {
+            throw new BadRequestException(String.format("Unable to delete object by id {}", id), e);
+        }
+    }
 }
