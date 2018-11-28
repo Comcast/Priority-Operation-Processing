87c818f83802861cec085b4a9a6ad42c34a03b43
David Caccia <david.caccia@theplatform.com>
2018-11-28 09:39:09 -0800

    DFH-3379 Implement PodConfig factory method; streamline static pod registry; implement single dfh pod termination string (already deployed in BaseHandlerEntryPoint)
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/handler/executor/": change = 533729]

--- handler/main/impl/src/main/java/com/theplatform/dfh/cp/handler/executor/impl/registry/podconfig/StaticPodConfigRegistryClient.java
+++ handler/main/impl/src/main/java/com/theplatform/dfh/cp/handler/executor/impl/registry/podconfig/StaticPodConfigRegistryClient.java
@@ -1,5 +1,6 @@
 package com.theplatform.dfh.cp.handler.executor.impl.registry.podconfig;
 
+import com.theplatform.dfh.cp.handler.base.BaseHandlerEntryPoint;
 import com.theplatform.dfh.cp.modules.kube.client.config.ConfigMapDetails;
 import com.theplatform.dfh.cp.modules.kube.client.config.PodConfig;
 import com.theplatform.dfh.cp.podconfig.registry.client.api.PodConfigRegistryClient;
@@ -12,88 +13,33 @@ import java.util.Map;
  */
 public class StaticPodConfigRegistryClient implements PodConfigRegistryClient
 {
+    private static final String DFH_SERVICE_ACCOUNT_NAME = "ffmpeg-service";
     private static Map<String, PodConfig> podConfigMap = new HashMap<>();
 
     static
     {
         podConfigMap.put("sample",
-            new PodConfig()
-                .setServiceAccountName("ffmpeg-service")
-                .setMemoryRequestCount("1000m")
-                .setCpuMinRequestCount("1000m")
-                .setCpuMaxRequestCount("1000m")
-                .setPodScheduledTimeoutMs(600000L)
-                .setReapCompletedPods(true)
-                .setPullAlways(true) // for now
-                .setImageName("docker-lab.repo.theplatform.com/fhsamp:1.0.0")
-                .setNamePrefix("dfh-samp")
-                .setEndOfLogIdentifier("SampleComplete")
-                .setConfigMapDetails(new ConfigMapDetails()
-                    .setConfigMapName("lab-main-t-aor-fhsamp-t01")
-                    .setMapKey("external-properties")
-                    .setMapPath("external.properties")
-                    .setVolumeName("config-volume")
-                    .setVolumeMountPath("/config"))
+                makeDfhBasePod("lab-main-t-aor-fhsamp-t01")
+                        .setImageName("docker-lab.repo.theplatform.com/fhsamp:1.0.0")
+                        .setNamePrefix("dfh-samp")
         );
 
         podConfigMap.put("analysis",
-            new PodConfig()
-                .setServiceAccountName("ffmpeg-service")
-                .setMemoryRequestCount("1000m")
-                .setCpuMinRequestCount("1000m")
-                .setCpuMaxRequestCount("1000m")
-                .setPodScheduledTimeoutMs(600000L)
-                .setReapCompletedPods(true)
-                .setPullAlways(true) // for now
-                .setImageName("docker-lab.repo.theplatform.com/fhami:1.0.0")
-                .setNamePrefix("dfh-analysis")
-                .setEndOfLogIdentifier("MediaInfoComplete")
-                .setConfigMapDetails(new ConfigMapDetails()
-                    .setConfigMapName("lab-main-t-aor-fhami-t01")
-                    .setMapKey("external-properties")
-                    .setMapPath("external.properties")
-                    .setVolumeName("config-volume")
-                    .setVolumeMountPath("/config"))
+                makeDfhBasePod("lab-main-t-aor-fhami-t01")
+                        .setImageName("docker-lab.repo.theplatform.com/fhami:1.0.0")
+                        .setNamePrefix("dfh-analysis")
         );
 
         podConfigMap.put("encode",
-            new PodConfig()
-                .setServiceAccountName("ffmpeg-service")
-                .setMemoryRequestCount("1000m")
-                .setCpuMinRequestCount("1000m")
-                .setCpuMaxRequestCount("1000m")
-                .setPodScheduledTimeoutMs(600000L)
-                .setReapCompletedPods(true)
-                .setPullAlways(true) // for now
-                .setImageName("docker-lab.repo.theplatform.com/fheff:1.0.0")
-                .setNamePrefix("dfh-encode")
-                .setEndOfLogIdentifier("ffmpeg_handler_end")
-                .setConfigMapDetails(new ConfigMapDetails()
-                    .setConfigMapName("lab-main-t-aor-fheff-t01")
-                    .setMapKey("external-properties")
-                    .setMapPath("external.properties")
-                    .setVolumeName("config-volume")
-                    .setVolumeMountPath("/config"))
+                makeDfhBasePod("lab-main-t-aor-fheff-t01")
+                        .setImageName("docker-lab.repo.theplatform.com/fheff:1.0.0")
+                        .setNamePrefix("dfh-encode")
         );
 
         podConfigMap.put("package",
-                new PodConfig()
-                        .setServiceAccountName("ffmpeg-service")
-                        .setMemoryRequestCount("1000m")
-                        .setCpuMinRequestCount("1000m")
-                        .setCpuMaxRequestCount("1000m")
-                        .setPodScheduledTimeoutMs(600000L)
-                        .setReapCompletedPods(true)
-                        .setPullAlways(true) // for now
-                        .setImageName("docker-lab.repo.theplatform.com/fhpkm:1.0.0")
-                        .setNamePrefix("dfh-package")
-                        .setEndOfLogIdentifier("package_handler_end")
-                        .setConfigMapDetails(new ConfigMapDetails()
-                                .setConfigMapName("lab-main-t-aor-fhpkm-t01")
-                                .setMapKey("external-properties")
-                                .setMapPath("external.properties")
-                                .setVolumeName("config-volume")
-                                .setVolumeMountPath("/config"))
+                makeDfhBasePod("lab-main-t-aor-fhpkm-t01")
+                .setImageName("docker-lab.repo.theplatform.com/fhpkm:1.0.0")
+                .setNamePrefix("dfh-package")
         );
     }
 
@@ -101,5 +47,25 @@ public class StaticPodConfigRegistryClient implements PodConfigRegistryClient
     public PodConfig getPodConfig(String type)
     {
         return podConfigMap.get(type);
+    }
+
+    public static PodConfig makeDfhBasePod(String configMapName)
+    {
+        ConfigMapDetails configMapDetails = new ConfigMapDetails()
+                .setConfigMapName(configMapName)
+                .setMapKey("external-properties")
+                .setMapPath("external.properties")
+                .setVolumeName("config-volume")
+                .setVolumeMountPath("/config");
+        return new PodConfig()
+                .setServiceAccountName(DFH_SERVICE_ACCOUNT_NAME)
+                .setMemoryRequestCount("1000m")
+                .setCpuMinRequestCount("1000m")
+                .setCpuMaxRequestCount("1000m")
+                .setPodScheduledTimeoutMs(600000L)
+                .setReapCompletedPods(true)
+                .setPullAlways(true)
+                .setEndOfLogIdentifier(BaseHandlerEntryPoint.DFH_POD_TERMINATION_STRING)
+                .setConfigMapDetails(configMapDetails);
     }
 }
