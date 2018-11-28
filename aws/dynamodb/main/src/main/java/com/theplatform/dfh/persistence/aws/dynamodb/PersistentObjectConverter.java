3870083b8aae4199f0b0555b9c2e0401430a3eed
kimberly.todd <a@b>
2018-11-28 12:54:01 -0800

    [CP] update persistent object converters to use super class with all the logic.  Also, persistent objects don't override all of the getters, just the ones that need different annotations
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/modules/Persistence/": change = 533755]

--- aws/dynamodb/main/src/main/java/com/theplatform/dfh/persistence/aws/dynamodb/PersistentObjectConverter.java
+++ aws/dynamodb/main/src/main/java/com/theplatform/dfh/persistence/aws/dynamodb/PersistentObjectConverter.java
@@ -1,15 +1,79 @@
 package com.theplatform.dfh.persistence.aws.dynamodb;
 
+import org.apache.commons.beanutils.BeanUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * T = api object class
  * S = persistent object class
  */
-public interface PersistentObjectConverter<T, S>
+public class PersistentObjectConverter<T, S>
 {
-    public abstract S getPersistentObject(T dataObject);
-    public abstract T getDataObject(S persistentObject);
+    private static final Logger logger = LoggerFactory.getLogger(PersistentObjectConverter.class);
 
-    public Class<S> getPersistentObjectClass();
-    public Class<T> getDataObjectClass();
+    Class dataClazz;
+    Class persistentClazz;
 
+    public PersistentObjectConverter(Class dataClazz, Class persistentClazz)
+    {
+        this.dataClazz = dataClazz;
+        this.persistentClazz = persistentClazz;
+    }
+
+    public Class getPersistentObjectClass()
+    {
+        return persistentClazz;
+    }
+
+    public Class getDataObjectClass()
+    {
+        return dataClazz;
+    }
+
+    public S getPersistentObject(T dataObject)
+    {
+        if (dataObject == null)
+            return null;
+
+        S persistentObject = (S) getNewIntance(persistentClazz);
+        return copy(persistentObject, dataObject) ? persistentObject : null;
+    }
+
+    public T getDataObject(S persistentObject)
+    {
+        if (persistentObject == null)
+            return null;
+
+        T dataObject = (T) getNewIntance(dataClazz);
+        return copy(dataObject, persistentObject) ? dataObject : null;
+    }
+
+    private boolean copy(Object dest, Object orig)
+    {
+        try
+        {
+            BeanUtils.copyProperties(dest, orig);
+            return true;
+        }
+        catch (InvocationTargetException | IllegalAccessException e)
+        {
+            logger.error("Unable to copy properties from data object to persistence", e);
+            return false;
+        }
+    }
+
+    Object getNewIntance(Class clazz)
+    {
+        try
+        {
+            return clazz.newInstance();
+        } catch (InstantiationException | IllegalAccessException e)
+        {
+            logger.debug("");
+            return null;
+        }
+    }
 }
