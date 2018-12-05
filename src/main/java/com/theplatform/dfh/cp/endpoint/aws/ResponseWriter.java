4c4d176786d26c5754014c9e0ab316372d2d0a9b
Tiffany Hallisey <Tiffany.Hallisey@theplatform.com>
2018-12-04 16:56:48 -0800

    babs -- Adding in the CORS headers temporarily
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/modules/BaseAWSEndpoint/main/": change = 533937]

--- src/main/java/com/theplatform/dfh/cp/endpoint/aws/ResponseWriter.java
+++ src/main/java/com/theplatform/dfh/cp/endpoint/aws/ResponseWriter.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Class for writing AWS responses to the OutputStream for use with implementations of RequestStreamHandler
@@ -23,7 +27,15 @@ public class ResponseWriter
     public void writeResponse(OutputStream outputStream, ObjectMapper objectMapper, int httpStatusCode, Object responseBodyObject) throws IOException
     {
         String responseBody = responseBodyObject == null ? null : objectMapper.writeValueAsString(responseBodyObject);
-        String response = objectMapper.writeValueAsString(new AWSLambdaStreamResponseObject(httpStatusCode, responseBody));
+        AWSLambdaStreamResponseObject responseObject = new AWSLambdaStreamResponseObject(httpStatusCode, responseBody);
+        //Putting the headers on the response for CORS @todo Remove if we don't use a browser for access to endpoints
+        Map<String, String> headers = new HashMap<>();
+        responseObject.setHeaders(headers);
+        headers.put("Access-Control-Allow-Headers", "origin,accept,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token");
+        headers.put("Access-Control-Allow-Origin", "*");
+        headers.put("Access-Control-Allow-Credentials", "true");
+        headers.put("Access-Control-Allow-Methods", "GET,OPTIONS");
+        String response = objectMapper.writeValueAsString(responseObject);
         OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
         writer.write(response);
         writer.close();
