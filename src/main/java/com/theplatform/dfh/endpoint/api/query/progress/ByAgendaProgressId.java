7630bb637c695ee41a7df387c0cb981eaeddead9
kimberly.todd <a@b>
2018-12-17 13:43:18 -0800

    get and delete AgendaProgress with OperationProgress
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/api/main/": change = 534287]

---
+++ src/main/java/com/theplatform/dfh/endpoint/api/query/progress/ByAgendaProgressId.java
@@ -0,0 +1,27 @@
+package com.theplatform.dfh.endpoint.api.query.progress;
+
+import com.theplatform.dfh.persistence.api.field.DataObjectField;
+import com.theplatform.dfh.persistence.api.query.Query;
+
+/**
+ */
+public class ByAgendaProgressId extends Query<String>
+{
+    private static final DataObjectField field = new DataObjectField("agendaProgressId");
+    public ByAgendaProgressId(String value)
+    {
+        if(value == null || value.isEmpty())
+        {
+            throw new IllegalArgumentException("ByAgendaProgressId query requires a non-empty value.");
+        }
+
+        setField(field);
+        setValue(value);
+        setCollection(false);
+    }
+
+    public static String fieldName()
+    {
+        return field.name();
+    }
+}
