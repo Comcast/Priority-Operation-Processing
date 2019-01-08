8385ddd79ca7be32c7e57a5cc666a116f32eecb9
Tim Stair <tim.stair@theplatform.com>
2019-01-07 16:16:08 -0800

    [DFH] Final reduction of the use of the HttpCPObjectClient. Still other things to be reduced...
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/impl/aws/main/": change = 534578]

--- src/main/java/com/theplatform/dfh/cp/endpoint/transformrequest/aws/TransformLambdaStreamEntry.java
+++ src/main/java/com/theplatform/dfh/cp/endpoint/transformrequest/aws/TransformLambdaStreamEntry.java
@@ -1,26 +1,45 @@
 package com.theplatform.dfh.cp.endpoint.transformrequest.aws;
 
+import com.theplatform.dfh.cp.api.Agenda;
 import com.theplatform.dfh.cp.api.TransformRequest;
+import com.theplatform.dfh.cp.api.progress.AgendaProgress;
+import com.theplatform.dfh.cp.api.progress.OperationProgress;
 import com.theplatform.dfh.cp.endpoint.TableEnvironmentVariableName;
+import com.theplatform.dfh.cp.endpoint.agenda.aws.persistence.DynamoDBAgendaPersisterFactory;
 import com.theplatform.dfh.cp.endpoint.aws.BaseAWSLambdaStreamEntry;
 import com.theplatform.dfh.cp.endpoint.aws.LambdaObjectRequest;
+import com.theplatform.dfh.cp.endpoint.operationprogress.aws.persistence.DynamoDBOperationProgressPersisterFactory;
+import com.theplatform.dfh.cp.endpoint.progress.aws.persistence.DynamoDBAgendaProgressPersisterFactory;
 import com.theplatform.dfh.cp.endpoint.transformrequest.TransformRequestProcessor;
+import com.theplatform.dfh.cp.scheduling.api.ReadyAgenda;
+import com.theplatform.dfh.endpoint.api.query.scheduling.ByCustomerId;
 import com.theplatform.dfh.http.idm.IDMHTTPUrlConnectionFactory;
 import com.theplatform.dfh.persistence.api.ObjectPersister;
+import com.theplatform.dfh.persistence.api.ObjectPersisterFactory;
+import com.theplatform.dfh.persistence.aws.dynamodb.AWSDynamoDBFactory;
 import com.theplatform.dfh.persistence.aws.dynamodb.DynamoDBCompressedObjectPersisterFactory;
+import com.theplatform.dfh.persistence.aws.dynamodb.DynamoDBConvertedObjectPersister;
+import com.theplatform.dfh.persistence.aws.dynamodb.TableIndexes;
+import com.theplatform.dfh.scheduling.aws.persistence.PersistentReadyAgendaConverter;
 
 /**
  * Main entry point class for the AWS TransformRequest endpoint
  */
 public class TransformLambdaStreamEntry extends BaseAWSLambdaStreamEntry<TransformRequest>
 {
+    private ObjectPersisterFactory<Agenda> agendaPersisterFactory;
+    private ObjectPersisterFactory<AgendaProgress> agendaProgressPersisterFactory;
+    private ObjectPersisterFactory<OperationProgress> operationProgressPersisterFactory;
+
     public TransformLambdaStreamEntry()
     {
         super(
             TransformRequest.class,
             new DynamoDBCompressedObjectPersisterFactory<>("id", TransformRequest.class)
         );
-
+        agendaPersisterFactory = new DynamoDBAgendaPersisterFactory();
+        agendaProgressPersisterFactory = new DynamoDBAgendaProgressPersisterFactory();
+        operationProgressPersisterFactory = new DynamoDBOperationProgressPersisterFactory();
     }
 
     @Override
@@ -31,10 +50,31 @@ public class TransformLambdaStreamEntry extends BaseAWSLambdaStreamEntry<Transfo
         {
             throw new RuntimeException("No Authorization node found. Unable to process request.");
         }
+        //@todo we need another non hardcoded way...
+        TableIndexes readyAgendaTableIndexes =
+            new TableIndexes().withIndex("customer_index", ByCustomerId.fieldName());
+        DynamoDBConvertedObjectPersister<ReadyAgenda> readyAgendaPersister = new DynamoDBConvertedObjectPersister<>
+            (
+                environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.READY_AGENDA),
+                "id",
+                new AWSDynamoDBFactory(),
+                ReadyAgenda.class,
+                new PersistentReadyAgendaConverter(),
+                readyAgendaTableIndexes
+            );
 
-        String agendaURL = environmentLookupUtils.getAPIEndpointURL(lambdaRequest, "agendaPath");
-        String progressURL = environmentLookupUtils.getAPIEndpointURL(lambdaRequest, "progressPath");
-        return new TransformRequestProcessor(objectPersister, new IDMHTTPUrlConnectionFactory(authHeader).setCid(lambdaRequest.getCID()), progressURL, agendaURL);
+        String insightURL = environmentLookupUtils.getAPIEndpointURL(lambdaRequest, "insightPath");
+        String customerURL = environmentLookupUtils.getAPIEndpointURL(lambdaRequest, "customerPath");
+
+        return new TransformRequestProcessor(
+            objectPersister,
+            agendaPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.AGENDA)),
+            agendaProgressPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.AGENDA_PROGRESS)),
+            operationProgressPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.OPERATION_PROGRESS)),
+            readyAgendaPersister,
+            new IDMHTTPUrlConnectionFactory(authHeader).setCid(lambdaRequest.getCID()),
+            insightURL,
+            customerURL);
     }
 
     @Override
