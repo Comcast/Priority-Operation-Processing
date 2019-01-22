6f373ae78d9baea1e294db974d2df3284486dd69
Tiffany Hallisey <Tiffany.Hallisey@theplatform.com>
2019-01-22 12:11:49 -0800

    babs -- Global visibility added to support MPX service users.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/modules/BaseEndpoint/main/": change = 534894]

--- src/main/java/com/theplatform/dfh/cp/endpoint/base/visibility/CustomerVisibilityFilter.java
+++ src/main/java/com/theplatform/dfh/cp/endpoint/base/visibility/CustomerVisibilityFilter.java
@@ -1,7 +1,8 @@
 package com.theplatform.dfh.cp.endpoint.base.visibility;
 
-import com.theplatform.dfh.endpoint.api.AuthorizationResponse;
 import com.theplatform.dfh.endpoint.api.ServiceRequest;
+import com.theplatform.dfh.endpoint.api.auth.AuthorizationResponse;
+import com.theplatform.dfh.endpoint.api.auth.DataVisibility;
 import com.theplatform.dfh.object.api.IdentifiedObject;
 
 import java.util.ArrayList;
@@ -14,15 +15,18 @@ public class CustomerVisibilityFilter<T extends IdentifiedObject, Req extends Se
     @Override
     public boolean isVisible(Req req, T object)
     {
-        return true;
-        /*if(req == null || object == null) return false;
+        if(req == null || object == null) return false;
 
         AuthorizationResponse authorizationResponse = req.getAuthorizationResponse();
-        if(authorizationResponse == null || authorizationResponse.isSuperUser()) return true;
+        if(authorizationResponse == null) return false;
+
+        //If the visibility is global, we are ok to see all data.
+        if(authorizationResponse.getVisibility() == DataVisibility.global) return true;
 
         Set<String> authorizedCustomers = authorizationResponse.getAllowedCustomerIds();
         if(authorizedCustomers == null) return false;
-        return authorizedCustomers.contains(object.getCustomerId());*/
+
+        return authorizedCustomers.contains(object.getCustomerId());
     }
 
     @Override
