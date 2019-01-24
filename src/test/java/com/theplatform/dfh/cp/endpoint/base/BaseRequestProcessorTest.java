70c5f23c6fdf6cfa16b232eb369c9ab8003b8eb6
kimberly.todd <a@b>
2019-01-24 10:49:37 -0800

    fix unit test to use ErrorResponse
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/modules/BaseEndpoint/main/": change = 535052]

--- src/test/java/com/theplatform/dfh/cp/endpoint/base/BaseRequestProcessorTest.java
+++ src/test/java/com/theplatform/dfh/cp/endpoint/base/BaseRequestProcessorTest.java
@@ -3,6 +3,7 @@ package com.theplatform.dfh.cp.endpoint.base;
 import com.theplatform.dfh.endpoint.api.BadRequestException;
 import com.theplatform.dfh.endpoint.api.ObjectNotFoundException;
 import com.theplatform.dfh.endpoint.api.data.DataObjectRequest;
+import com.theplatform.dfh.endpoint.api.data.DataObjectResponse;
 import com.theplatform.dfh.endpoint.api.data.DefaultDataObjectRequest;
 import com.theplatform.dfh.endpoint.api.data.query.ByTitle;
 import com.theplatform.dfh.object.api.IdentifiedObject;
@@ -10,6 +11,7 @@ import com.theplatform.dfh.persistence.api.ObjectPersister;
 import com.theplatform.dfh.persistence.api.PersistenceException;
 import com.theplatform.dfh.persistence.api.query.Query;
 import org.mockito.Mockito;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Collections;
@@ -19,14 +21,16 @@ public class BaseRequestProcessorTest
 {
     ObjectPersister<SimpleObject> objectPersister = Mockito.mock(ObjectPersister.class);
 
-    @Test(expectedExceptions = ObjectNotFoundException.class)
+    @Test
     public void testGetByQueryBad() throws BadRequestException, PersistenceException
     {
         TestBaseProcessor processor = new TestBaseProcessor(objectPersister);
         Mockito.when(objectPersister.retrieve(Mockito.anyList())).thenThrow(PersistenceException.class);
         DefaultDataObjectRequest<SimpleObject> request = new DefaultDataObjectRequest<>();
         request.setQueries(Collections.singletonList(new ByTitle("xyz")));
-        processor.handleGET(request);
+        DataObjectResponse<SimpleObject> response = processor.handleGET(request);
+        Assert.assertTrue(response.isError());
+        Assert.assertEquals(response.getErrorResponse().getTitle(), "ObjectNotFoundException");
     }
 
     private class TestBaseProcessor extends DataObjectRequestProcessor<SimpleObject>
