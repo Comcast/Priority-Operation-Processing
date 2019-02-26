b63bca7b66713dd2e3ee83aeec168cdb97334c80
Tim Stair <tim.stair@theplatform.com>
2019-02-26 09:00:26 -0800

    [DFH] Update webfission for linkId query (still a little suspect due to the case on linkId)
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/web/": change = 535822]

--- src/main/resources/login.js
+++ src/main/resources/login.js
@@ -1,9 +1,6 @@
-function getQueryURL(server, endpoint)
+function getQueryURL(server, endpoint, querySelectedType, queryValue)
 {
     var endpointURL = getEndpointURL(server, endpoint);
-    var queryElement = document.getElementById("query");
-    var querySelectedType = queryElement.options[queryElement.selectedIndex].value;
-    var queryValue = document.getElementById("query_value").value;
     if(querySelectedType == "byId")
     {
         return queryValue == "" ? endpointURL : endpointURL +"/" +queryValue;
@@ -27,6 +24,13 @@ function authorizeWithLambda(e) {
         alert ("Please enter your password");
         return;
     }
+
+    var queryElement = document.getElementById("query");
+    var querySelectedType = queryElement.options[queryElement.selectedIndex].value;
+    var queryValue = document.getElementById("query_value").value;
+
+    queryValue = encodeURIComponent(queryValue);
+
     var username = $("#mpx_username").val();
     var password = $("#mpx_password").val();
     var accountId = $("#mpx_account").val();
@@ -71,7 +75,7 @@ function authorizeWithLambda(e) {
             }
             $.ajax({
                 type: "GET",
-                url: getQueryURL(server, endpoint),
+                url: getQueryURL(server, endpoint, querySelectedType, queryValue),
                 crossDomain: true,
                 jsonp: true,
                 contentType: "application/json",
@@ -97,89 +101,3 @@ function authorizeWithLambda(e) {
         }
     });
 }
-
-
-// function authorizeWithLambdaForLinkId(e) {
-//     e.preventDefault();
-//
-//     if ($("#mpx_username").val()=="") {
-//         alert ("Please enter your user id");
-//         return;
-//     }
-//     if ($("#mpx_password").val()=="") {
-//         alert ("Please enter your password");
-//         return;
-//     }
-//     var username = $("#mpx_username").val();
-//     var password = $("#mpx_password").val();
-//     var accountId = $("#mpx_account").val();
-//
-//     var CID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-//     var authUserPasswordHeader = "Basic " +  btoa(username + ":" + password);
-//
-//     var targetServerIndex = $("#target_server").val();
-//     var targetEndpointIndex = $("#target_endpoint").val();
-//
-//     var server = servers[targetServerIndex];
-//     var agendaEndpoint = endpoints["Agenda"];
-//
-//     getEndpointURL(server, agendaEndpoint);
-//
-//     // agenda progress: get by linkId
-//
-//     //alert(queryURL);
-//
-//     //    {"signInResponse":{"token":"XYZ","userId":"http://identity.auth.test.corp.theplatform.com/idm/data/User/mpx/ZZZ","userName":"me@me.com","duration":86400000,"idleTimeout":14400000}}
-//
-//     document.getElementById("response").value = "";
-//
-//     $.ajax({
-//         type: "POST",
-//         url: server.endpointIDMURL,
-//         crossDomain: "true",
-//         cache:false,
-//         dataType:"json",
-//         jsonp: false,
-//
-//         headers: {
-//             "Authorization": authUserPasswordHeader,
-//             "Accept": "*/*",
-//             "Content-Type": "application/json"
-//         },
-//         data: "{\"signIn\": {\"duration\": 86400000, \"idleTimeout\": 14400000}}",
-//
-//         success: function (idmResponse) {
-//             console.log(idmResponse);
-//             if(idmResponse.isException == true)
-//             {
-//                 alert(idmResponse.description);
-//                 return;
-//             }
-//             $.ajax({
-//                 type: "GET",
-//                 url: getQueryURL(targetServerIndex, targetEndpointIndex),
-//                 crossDomain: true,
-//                 jsonp: true,
-//                 contentType: "application/json",
-//                 headers: {
-//                     'Authorization': "Basic " +btoa(accountId +":" +idmResponse.signInResponse.token),
-//                     'Content-Type': "application/json",
-//                     'X-thePlatform-cid': CID
-//                 },
-//                 success: function (response) {
-//                     document.getElementById("response").value += JSON.stringify(response, null, 2);
-//                     $('#json-renderer').jsonViewer(response);
-//                 },
-//                 error: function (response) {
-//                     // show an error message
-//                     $('#json-renderer').jsonViewer(response);
-//                     alert("UnSuccessfull CID '" +CID +"'");
-//                 }
-//             });
-//         },
-//         error: function () {
-//             // show an error message
-//             alert("UnSuccessfull");
-//         }
-//     });
-// }
