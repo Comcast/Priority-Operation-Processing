624884aaae1854a64e31615f8cc66e758a668fd6
Tim Stair <tim.stair@theplatform.com>
2019-02-25 20:41:28 -0800

    [DFH] Update web client. Code sharing...
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/web/": change = 535810]

--- src/main/resources/login.js
+++ src/main/resources/login.js
@@ -1,3 +1,21 @@
+function getQueryURL(server, endpoint)
+{
+    var endpointURL = getEndpointURL(server, endpoint);
+    var queryElement = document.getElementById("query");
+    var querySelectedType = queryElement.options[queryElement.selectedIndex].value;
+    var queryValue = document.getElementById("query_value").value;
+    if(querySelectedType == "byId")
+    {
+        return queryValue == "" ? endpointURL : endpointURL +"/" +queryValue;
+    }
+    else if(querySelectedType == "other")
+    {
+        return queryValue == "" ? endpointURL : endpointURL +"?" +queryValue;
+    }
+
+    return queryValue == "" ? endpointURL : endpointURL +"?" +querySelectedType +"=" +queryValue;
+}
+
 function authorizeWithLambda(e) {
     e.preventDefault();
 
@@ -16,9 +34,22 @@ function authorizeWithLambda(e) {
     var CID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     var authUserPasswordHeader = "Basic " +  btoa(username + ":" + password);
 
+    var targetServerIndex = $("#target_server").val();
+    var targetEndpointIndex = $("#target_endpoint").val();
+
+    var server = servers[targetServerIndex];
+    var endpoint = endpoints[targetEndpointIndex];
+
+    //var queryURL = getQueryURL(server, endpoint);
+    //alert(queryURL);
+
+    //    {"signInResponse":{"token":"XYZ","userId":"http://identity.auth.test.corp.theplatform.com/idm/data/User/mpx/ZZZ","userName":"me@me.com","duration":86400000,"idleTimeout":14400000}}
+
+    document.getElementById("response").value = "";
+
     $.ajax({
         type: "POST",
-        url: $("#mpx_idm_url").val(),
+        url: server.endpointIDMURL,
         crossDomain: "true",
         cache:false,
         dataType:"json",
@@ -31,9 +62,6 @@ function authorizeWithLambda(e) {
         },
         data: "{\"signIn\": {\"duration\": 86400000, \"idleTimeout\": 14400000}}",
 
-
-//    {"signInResponse":{"token":"XYZ","userId":"http://identity.auth.test.corp.theplatform.com/idm/data/User/mpx/ZZZ","userName":"me@me.com","duration":86400000,"idleTimeout":14400000}}
-
         success: function (idmResponse) {
             console.log(idmResponse);
             if(idmResponse.isException == true)
@@ -43,7 +71,7 @@ function authorizeWithLambda(e) {
             }
             $.ajax({
                 type: "GET",
-                url: getEndpointURL(),
+                url: getQueryURL(server, endpoint),
                 crossDomain: true,
                 jsonp: true,
                 contentType: "application/json",
@@ -68,23 +96,90 @@ function authorizeWithLambda(e) {
             alert("UnSuccessfull");
         }
     });
-
-
-    function getEndpointURL()
-    {
-        var endpointURL = $("#endpointURL").val();
-        var queryElement = document.getElementById("query");
-        var querySelectedType = queryElement.options[queryElement.selectedIndex].value;
-        var queryValue = document.getElementById("query_value").value;
-        if(querySelectedType == "byId")
-        {
-            return queryValue == "" ? endpointURL : endpointURL +"/" +queryValue;
-        }
-        else if(querySelectedType == "other")
-        {
-            return queryValue == "" ? endpointURL : endpointURL +"?" +queryValue;
-        }
-
-        return queryValue == "" ? endpointURL : endpointURL +"?" +querySelectedType +"=" +queryValue;
-    }
 }
+
+
+// function authorizeWithLambdaForLinkId(e) {
+//     e.preventDefault();
+//
+//     if ($("#mpx_username").val()=="") {
+//         alert ("Please enter your user id");
+//         return;
+//     }
+//     if ($("#mpx_password").val()=="") {
+//         alert ("Please enter your password");
+//         return;
+//     }
+//     var username = $("#mpx_username").val();
+//     var password = $("#mpx_password").val();
+//     var accountId = $("#mpx_account").val();
+//
+//     var CID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+//     var authUserPasswordHeader = "Basic " +  btoa(username + ":" + password);
+//
+//     var targetServerIndex = $("#target_server").val();
+//     var targetEndpointIndex = $("#target_endpoint").val();
+//
+//     var server = servers[targetServerIndex];
+//     var agendaEndpoint = endpoints["Agenda"];
+//
+//     getEndpointURL(server, agendaEndpoint);
+//
+//     // agenda progress: get by linkId
+//
+//     //alert(queryURL);
+//
+//     //    {"signInResponse":{"token":"XYZ","userId":"http://identity.auth.test.corp.theplatform.com/idm/data/User/mpx/ZZZ","userName":"me@me.com","duration":86400000,"idleTimeout":14400000}}
+//
+//     document.getElementById("response").value = "";
+//
+//     $.ajax({
+//         type: "POST",
+//         url: server.endpointIDMURL,
+//         crossDomain: "true",
+//         cache:false,
+//         dataType:"json",
+//         jsonp: false,
+//
+//         headers: {
+//             "Authorization": authUserPasswordHeader,
+//             "Accept": "*/*",
+//             "Content-Type": "application/json"
+//         },
+//         data: "{\"signIn\": {\"duration\": 86400000, \"idleTimeout\": 14400000}}",
+//
+//         success: function (idmResponse) {
+//             console.log(idmResponse);
+//             if(idmResponse.isException == true)
+//             {
+//                 alert(idmResponse.description);
+//                 return;
+//             }
+//             $.ajax({
+//                 type: "GET",
+//                 url: getQueryURL(targetServerIndex, targetEndpointIndex),
+//                 crossDomain: true,
+//                 jsonp: true,
+//                 contentType: "application/json",
+//                 headers: {
+//                     'Authorization': "Basic " +btoa(accountId +":" +idmResponse.signInResponse.token),
+//                     'Content-Type': "application/json",
+//                     'X-thePlatform-cid': CID
+//                 },
+//                 success: function (response) {
+//                     document.getElementById("response").value += JSON.stringify(response, null, 2);
+//                     $('#json-renderer').jsonViewer(response);
+//                 },
+//                 error: function (response) {
+//                     // show an error message
+//                     $('#json-renderer').jsonViewer(response);
+//                     alert("UnSuccessfull CID '" +CID +"'");
+//                 }
+//             });
+//         },
+//         error: function () {
+//             // show an error message
+//             alert("UnSuccessfull");
+//         }
+//     });
+// }
---
+++ src/main/resources/servers.js
@@ -0,0 +1,63 @@
+var labIDMURL = "https://identity.auth.test.corp.theplatform.com/idm/web/Authentication?form=json&schema=1.1";
+var prodIDMURL = "https://identity.auth.theplatform.com/idm/web/Authentication?form=json&schema=1.1";
+var servers =
+        [
+            {
+                name:"Twinkle (Lab)",
+                endpointServerURL:"https://g9solclg15.execute-api.us-west-2.amazonaws.com",
+                endpointStage:"dev",
+                endpointIDMURL:labIDMURL
+            },
+            {
+                name:"Rage (Prod)",
+                endpointServerURL:"https://fhm5nnh1n3.execute-api.us-west-2.amazonaws.com",
+                endpointStage:"SEA1",
+                endpointIDMURL:prodIDMURL
+            }
+        ];
+
+var endpointPrefix = "/dfh/idm/";
+
+var endpoints =
+        [
+            {
+                name:"Agenda",
+                path:"agenda"
+            },
+            {
+                name:"Agenda Progress",
+                path:"progress/agenda"
+            },
+            {
+                name:"Customer",
+                path:"customer"
+            },
+            {
+                name:"Insight",
+                path:"insight"
+            },
+            {
+                name:"Operation Progress",
+                path:"progress/operation"
+            },
+            {
+                name:"Resource Pool",
+                path:"resourcepool"
+            },
+            {
+                name:"Transform",
+                path:"transform"
+            }
+        ];
+
+function getEndpointURL(server, endpoint) {
+    return server.endpointServerURL + "/" + server.endpointStage + endpointPrefix + endpoint.path;
+}
+
+var endpointsByName = [];
+
+endpoints.forEach(addEndpointByName);
+
+function addEndpointByName(item, index) {
+    endpointsByName[item.name] = item;
+}
