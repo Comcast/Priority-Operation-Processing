2e4513c7cacaf3d53d4785344d30d9d3ccead6a7
Tim Stair <tim.stair@theplatform.com>
2019-03-02 09:08:42 -0800

    [Fission] Added ldap prefix file params (necessary step to support adjusting filenames on the task callback)
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/api/": change = 535890]

--- main/src/main/java/com/theplatform/dfh/cp/api/params/FileParamKey.java
+++ main/src/main/java/com/theplatform/dfh/cp/api/params/FileParamKey.java
@@ -2,7 +2,9 @@ package com.theplatform.dfh.cp.api.params;
 
 public enum FileParamKey implements ParamKey
 {
-    fileSize;
+    fileSize,
+    ldapOriginalPrefix,
+    ldapRemappedPrefix;
 
     public String getKey()
     {
