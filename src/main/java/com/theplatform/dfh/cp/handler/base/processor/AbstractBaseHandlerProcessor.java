0285bb7d0fed719b1853706f4825c7d9594c6ddd
David Caccia <david.caccia@theplatform.com>
2019-03-27 07:09:11 -0800

    Extract exec metadata logic from base classes.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/handler/modules/base/main/": change = 536228]

--- src/main/java/com/theplatform/dfh/cp/handler/base/processor/AbstractBaseHandlerProcessor.java
+++ src/main/java/com/theplatform/dfh/cp/handler/base/processor/AbstractBaseHandlerProcessor.java
@@ -4,10 +4,15 @@ import com.theplatform.dfh.cp.handler.base.context.BaseOperationContext;
 import com.theplatform.dfh.cp.handler.field.api.args.MetaData;
 import com.theplatform.dfh.cp.handler.field.retriever.LaunchDataWrapper;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class AbstractBaseHandlerProcessor<L extends LaunchDataWrapper, C extends BaseOperationContext<L> > implements HandlerProcessor, MetaData<Object>
 {
     protected  L launchDataWrapper;
     protected  C operationContext;
+    private Map<String,Object> handlerMetadata = new HashMap<>();
+
 
     public AbstractBaseHandlerProcessor(C operationContext)
     {
@@ -33,5 +38,24 @@ public abstract class AbstractBaseHandlerProcessor<L extends LaunchDataWrapper, 
     public C getOperationContext()
     {
         return operationContext;
+    }
+
+
+    /**
+     * Not all subclasses need to pass along metadata, so is only called when needed.
+     * @param metaData
+     */
+    protected void assignHandlerMetadata(MetaData metaData)
+    {
+        if(!metaData.getMetadata().isEmpty())
+        {
+            handlerMetadata.putAll(metaData.getMetadata());
+        }
+    }
+
+    @Override
+    public Map<String, Object> getMetadata()
+    {
+        return handlerMetadata;
     }
 }
