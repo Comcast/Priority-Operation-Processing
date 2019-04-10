7afc983566d2b2c732278d595da321c4af40d174
Tim Stair <tim.stair@theplatform.com>
2019-04-10 07:39:22 -0800

    [Fission] Added general exception handler to the puller.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/handler/puller/": change = 536506]

--- handler/main/impl/src/main/java/com/theplatform/dfh/cp/handler/puller/impl/context/PullerContext.java
+++ handler/main/impl/src/main/java/com/theplatform/dfh/cp/handler/puller/impl/context/PullerContext.java
@@ -3,15 +3,25 @@ package com.theplatform.dfh.cp.handler.puller.impl.context;
 import com.theplatform.dfh.cp.handler.base.context.BaseOperationContext;
 import com.theplatform.dfh.cp.handler.puller.impl.config.PullerLaunchDataWrapper;
 import com.theplatform.dfh.cp.handler.puller.impl.executor.LauncherFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PullerContext extends BaseOperationContext<PullerLaunchDataWrapper>
 {
+    private static Logger logger = LoggerFactory.getLogger(PullerContext.class);
+
     private LauncherFactory launcherFactory;
 
     public PullerContext(PullerLaunchDataWrapper launchDataWrapper, LauncherFactory launcherFactory)
     {
         super(launchDataWrapper);
         this.launcherFactory = launcherFactory;
+    }
+
+    @Override
+    public void processUnhandledException(String s, Exception e)
+    {
+        logger.error(s, e);
     }
 
     public LauncherFactory getLauncherFactory()
