88277b4ed98ff945278ac91a1bd2d40929b682e9
Tim Stair <tim.stair@theplatform.com>
2019-05-10 12:44:08 -0800

    [Fission] Added utility function for splitting a collection via streams (common for batched producer/consumer)
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/modules/ProducerConsumer/main/": change = 537127]

--- src/main/java/com/theplatform/com/dfh/modules/sync/util/InstantUtil.java
+++ src/main/java/com/theplatform/com/dfh/modules/sync/util/InstantUtil.java
@@ -4,11 +4,22 @@ import java.time.Instant;
 
 public class InstantUtil
 {
+    /**
+     * Determines if "now" is after or equal to the specified end Instant
+     * @param endTime The time to compare now to
+     * @return true if now is after or equal to the specified end Instant, false otherwise
+     */
     public static boolean isNowAfterOrEqual(Instant endTime)
     {
         return isAfterOrEqual(Instant.now(), endTime);
     }
 
+    /**
+     * Determines if the source Instant is after or equal to the specified target Instant
+     * @param source The source Instant
+     * @param target The target Instant
+     * @return true if source is after or equal to the specified target Instant, false otherwise
+     */
     public static boolean isAfterOrEqual(Instant source, Instant target)
     {
         return source.equals(target)
---
+++ src/test/java/com/theplatform/com/dfh/modules/sync/util/CollectionUtilTest.java
@@ -0,0 +1,56 @@
+package com.theplatform.com.dfh.modules.sync.util;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CollectionUtilTest
+{
+    @DataProvider
+    public Object[][] splitProvider()
+    {
+        return new Object[][]
+            {
+                { createList(0), 1},
+                { createList(1), 1},
+                { createList(11), 4},
+                { createList(25), 5},
+                { createList(26), 5},
+            };
+    }
+
+    @Test(dataProvider = "splitProvider")
+    public void testSplit(List<Integer> list, final int SUBSET_SIZE)
+    {
+        final int EXPECTED_SET_COUNT = (int)Math.ceil((double)list.size() / (double)SUBSET_SIZE);
+        Collection<List<Integer>> splitCollection = CollectionUtil.split(list, SUBSET_SIZE);
+        Assert.assertEquals(splitCollection.size(), EXPECTED_SET_COUNT);
+    }
+
+    @DataProvider
+    public Object[][] splitInvalidInputProvider()
+    {
+        return new Object[][]
+            {
+                { createList(0), -1},
+                { createList(0), 0},
+                { null, 1},
+            };
+    }
+
+    @Test(dataProvider = "splitInvalidInputProvider")
+    public void testSplitInvalidInput(List<Integer> list, final int SUBSET_SIZE)
+    {
+        Assert.assertNull(CollectionUtil.split(list, SUBSET_SIZE));
+    }
+
+    public List<Integer> createList(int size)
+    {
+        return IntStream.range(0, size).boxed().collect(Collectors.toList());
+    }
+}
