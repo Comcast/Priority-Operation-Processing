d27ead0dd980062d23bc06711849f6ac8f3d18e9
David Caccia <david.caccia@theplatform.com>
2019-05-20 14:39:43 -0800

    Initial checkin for command builder fission
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/modules/command_builder_fission/": change = 537331]

---
+++ src/main/java/com/theplatform/dfh/cp/handler/command/builder/fission/s3/DfhS3KeyConverter.java
@@ -0,0 +1,21 @@
+package com.theplatform.dfh.cp.handler.command.builder.fission.s3;
+
+import com.theplatform.commandbuilder.impl.build.handler.ConnectionKeys;
+import com.theplatform.commandbuilder.impl.build.handler.KeyConversion;
+import com.theplatform.commandbuilder.impl.build.handler.NoopConnectKey;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class DfhS3KeyConverter implements KeyConversion
+{
+    private static ConnectionKeys notFound = new NoopConnectKey();
+
+    public ConnectionKeys convertKey(String keyToConvert)
+    {
+        Optional<ConnectionKeys> optionalKey = Arrays.stream(S3ConnectConvert.values())
+                .filter(converter -> converter.name().equals(keyToConvert))
+                .map(S3ConnectConvert::getKey).findFirst();
+        return optionalKey.orElseGet(() -> notFound);
+    }
+}
