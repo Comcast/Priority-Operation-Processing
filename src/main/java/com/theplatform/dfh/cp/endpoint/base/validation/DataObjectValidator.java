f67136945730d17f98915388aacc7534e43f8742
Tim Stair <tim.stair@theplatform.com>
2019-06-06 15:08:49 -0800

    [Endpoint] Moved exception messages in the basic validator to constants.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/modules/BaseEndpoint/main/": change = 537652]

--- src/main/java/com/theplatform/dfh/cp/endpoint/base/validation/DataObjectValidator.java
+++ src/main/java/com/theplatform/dfh/cp/endpoint/base/validation/DataObjectValidator.java
@@ -10,6 +10,10 @@ import com.theplatform.dfh.object.api.IdentifiedObject;
  */
 public class DataObjectValidator<T extends IdentifiedObject, R extends DataObjectRequest<T>> extends DefaultRequestValidator<R>
 {
+    private static final String UNABLE_TO_PROCESS_NULL_EXCEPTION = "Unable to %1$s a null object";
+    private static final String UNABLE_TO_PUT_WITHOUT_ID_EXCEPTION = "Unable to PUT an object without specifying an id";
+    private static final String MISMATCHED_PUT_ID_EXCEPTION = "Mismatched Id in URL and on input object: URLId: %1$s and ObjectId: %2$s";
+
     @Override
     public void validatePOST(R request)
     {
@@ -24,19 +28,17 @@ public class DataObjectValidator<T extends IdentifiedObject, R extends DataObjec
         validateObjectRequest(request, "PUT");
 
         if(request.getId() == null)
-            throw new ValidationException("Unable to PUT an object without specifying an id");
+            throw new ValidationException(UNABLE_TO_PUT_WITHOUT_ID_EXCEPTION);
 
         T object = request.getDataObject();
         if(object.getId() != null && !object.getId().equals(request.getId()))
-            throw new ValidationException(String.format("Mismatched Id in URL and on input object: URLId: %1$s and ObjectId: %2$s",
-                request.getId(),
-                object.getId()));
+            throw new ValidationException(String.format(MISMATCHED_PUT_ID_EXCEPTION, request.getId(), object.getId()));
     }
 
     protected void validateObjectRequest(R request, String requestType)
     {
         T object = request.getDataObject();
         if(object == null)
-            throw new ValidationException(String.format("Unable to %1$s a null object", requestType));
+            throw new ValidationException(String.format(UNABLE_TO_PROCESS_NULL_EXCEPTION, requestType));
     }
 }
