0b4a34084fbbf9fefe204a69b51dd7c945c6195c
Tiffany Hallisey <Tiffany.Hallisey@theplatform.com>
2019-07-11 09:59:04 -0800

    babs -- Graphite uses new configuration properties
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/modules/monitor/": change = 538101]

--- graphite/main/src/main/java/com/theplatform/dfh/cp/modules/monitor/graphite/GraphiteMetricReporterFactory.java
+++ graphite/main/src/main/java/com/theplatform/dfh/cp/modules/monitor/graphite/GraphiteMetricReporterFactory.java
@@ -3,6 +3,7 @@ package com.theplatform.dfh.cp.modules.monitor.graphite;
 import com.codahale.metrics.*;
 import com.codahale.metrics.graphite.Graphite;
 import com.codahale.metrics.graphite.GraphiteReporter;
+import com.theplatform.dfh.cp.modules.monitor.config.ConfigurationProperties;
 import com.theplatform.dfh.cp.modules.monitor.metric.MetricReporterFactory;
 
 import java.net.InetSocketAddress;
@@ -14,14 +15,14 @@ import java.util.concurrent.TimeUnit;
  */
 public class GraphiteMetricReporterFactory implements MetricReporterFactory
 {
-    private GraphiteConfiguration metricsConfiguration;
+    private ConfigurationProperties metricsConfiguration;
     private MetricFilter metricFilter = MetricFilter.ALL;
 
     public GraphiteMetricReporterFactory(Properties properties)
     {
-        this.metricsConfiguration = new GraphiteConfiguration(properties);
+        this.metricsConfiguration = ConfigurationProperties.from(properties, new GraphiteConfigKeys());
     }
-    public GraphiteMetricReporterFactory(GraphiteConfiguration configuration)
+    public GraphiteMetricReporterFactory(ConfigurationProperties configuration)
     {
         this.metricsConfiguration = configuration;
     }
@@ -30,13 +31,18 @@ public class GraphiteMetricReporterFactory implements MetricReporterFactory
     public ScheduledReporter register(MetricRegistry metricRegistry)
     {
         // set up "standard" graphite push for metrics
-        InetSocketAddress metricsServerLocation =
-            new InetSocketAddress( metricsConfiguration.getGraphiteEndpoint(), metricsConfiguration.getGraphitePort() );
+        final String endpoint = metricsConfiguration.get(GraphiteConfigKeys.ENDPOINT);
+        if(endpoint == null)
+            throw new IllegalArgumentException("No endpoint defined for Graphite reporter. Missing property " +GraphiteConfigKeys.ENDPOINT.getPropertyKey());
+
+        final Integer port = metricsConfiguration.get(GraphiteConfigKeys.PORT);
+
+        InetSocketAddress metricsServerLocation = new InetSocketAddress(endpoint, port);
         Graphite standardPusher = new Graphite(metricsServerLocation);
         GraphiteReporter reporter = GraphiteReporter.forRegistry(metricRegistry)
             .convertRatesTo(TimeUnit.MILLISECONDS)
             // N.B 1m is "somewhat" arbitrary - it controls the retention period of the metrics for comparision purposes
-            .prefixedWith( metricsConfiguration.getGraphitePath() )
+            .prefixedWith(metricsConfiguration.get(GraphiteConfigKeys.PATH))
             .filter(metricFilter)
             .build(standardPusher);
         //We need to override the Graphite Reporter to catch all exceptions from preventing our applications to fail.
@@ -47,7 +53,7 @@ public class GraphiteMetricReporterFactory implements MetricReporterFactory
     @Override
     public int getReportIntervalInMilli()
     {
-        return metricsConfiguration.getMetricsReportInterval();
+        return metricsConfiguration.get(GraphiteConfigKeys.REPORT_FREQUENCY);
     }
 
     public void setMetricFilter(MetricFilter metricFilter)
