33c8c030bee4e634c5efb54165883664d3c5a07e
Tim Stair <tim.stair@theplatform.com>
2019-07-17 08:22:00 -0800

    [Fission] Endpoint updated get progress summary to use the unified request processing (default error handling and simpler validation flow).
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/impl/aws/main/": change = 538225]

--- src/main/java/com/theplatform/dfh/cp/endpoint/progress/service/aws/ProgressServiceLambdaStreamEntry.java
+++ src/main/java/com/theplatform/dfh/cp/endpoint/progress/service/aws/ProgressServiceLambdaStreamEntry.java
@@ -1,37 +1,28 @@
 package com.theplatform.dfh.cp.endpoint.progress.service.aws;
 
-import com.amazonaws.services.lambda.runtime.Context;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.theplatform.dfh.cp.endpoint.TableEnvironmentVariableName;
 import com.theplatform.dfh.cp.endpoint.agenda.aws.persistence.DynamoDBAgendaPersisterFactory;
+import com.theplatform.dfh.cp.endpoint.aws.AbstractLambdaStreamEntry;
 import com.theplatform.dfh.cp.endpoint.aws.EnvironmentLookupUtils;
-import com.theplatform.dfh.cp.endpoint.aws.JsonRequestStreamHandler;
 import com.theplatform.dfh.cp.endpoint.aws.LambdaRequest;
-import com.theplatform.dfh.cp.endpoint.aws.ResponseWriter;
+import com.theplatform.dfh.cp.endpoint.base.RequestProcessor;
 import com.theplatform.dfh.cp.endpoint.operationprogress.aws.persistence.DynamoDBOperationProgressPersisterFactory;
 import com.theplatform.dfh.cp.endpoint.progress.aws.persistence.DynamoDBAgendaProgressPersisterFactory;
 import com.theplatform.dfh.cp.endpoint.progress.service.ProgressSummaryRequestProcessor;
 import com.theplatform.dfh.cp.endpoint.progress.service.api.ProgressSummaryRequest;
 import com.theplatform.dfh.cp.endpoint.progress.service.api.ProgressSummaryResponse;
-import com.theplatform.dfh.cp.modules.jsonhelper.JsonHelper;
-import com.theplatform.dfh.endpoint.api.DefaultServiceRequest;
-import com.theplatform.dfh.endpoint.api.ServiceRequest;
+import com.theplatform.dfh.endpoint.api.BadRequestException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-public class ProgressServiceLambdaStreamEntry implements JsonRequestStreamHandler
+public class ProgressServiceLambdaStreamEntry extends AbstractLambdaStreamEntry<ProgressSummaryResponse, LambdaRequest<ProgressSummaryRequest>>
 {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    private static final JsonHelper jsonHelper = new JsonHelper();
     private final EnvironmentLookupUtils environmentLookupUtils = new EnvironmentLookupUtils();
     private DynamoDBAgendaProgressPersisterFactory agendaProgressPersisterFactory;
     private DynamoDBAgendaPersisterFactory agendaPersisterFactory;
     private DynamoDBOperationProgressPersisterFactory operationProgressPersisterFactory;
-    private ResponseWriter responseWriter = new ResponseWriter();
 
     public ProgressServiceLambdaStreamEntry()
     {
@@ -41,64 +32,18 @@ public class ProgressServiceLambdaStreamEntry implements JsonRequestStreamHandle
     }
 
     @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException
+    public RequestProcessor getRequestProcessor(LambdaRequest<ProgressSummaryRequest> serviceRequest)
     {
-        // this is immediately made available for subclasses
-        JsonNode rootRequestNode = jsonHelper.getObjectMapper().readTree(inputStream);
-        handleRequest(rootRequestNode, outputStream, context);
+        return new ProgressSummaryRequestProcessor(
+            agendaProgressPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(serviceRequest, TableEnvironmentVariableName.AGENDA_PROGRESS)),
+            operationProgressPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(serviceRequest, TableEnvironmentVariableName.OPERATION_PROGRESS)),
+            agendaPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(serviceRequest, TableEnvironmentVariableName.AGENDA))
+        );
     }
 
     @Override
-    public void handleRequest(JsonNode inputStreamNode, OutputStream outputStream, Context context) throws IOException
+    public LambdaRequest<ProgressSummaryRequest> getRequest(JsonNode node) throws BadRequestException
     {
-        logger.info("Progress Service endpoint request received.");
-        LambdaRequest<ProgressSummaryRequest> lambdaRequest = new LambdaRequest<>(inputStreamNode, ProgressSummaryRequest.class);
-
-        ProgressSummaryResponse responseObject = null;
-        int httpStatusCode = 200;
-
-        if(lambdaRequest.getAuthorizationHeader() == null)
-        {
-            throw new RuntimeException("No Authorization node found. Unable to process request.");
-        }
-
-        String httpMethod = lambdaRequest.getHTTPMethod("UNKNOWN").toUpperCase();
-        try
-        {
-            switch (httpMethod)
-            {
-                case "POST":
-                    responseObject = handlePost(lambdaRequest);
-                    break;
-                default:
-                    // todo: some bad response code
-                    httpStatusCode = 405;
-                    logger.warn("Unsupported method type {}.", httpMethod);
-            }
-        }
-        catch(Exception e)
-        {
-            httpStatusCode = 500;
-            responseObject = null; // TODO: need a standard error body?
-            logger.error("Error retrieving progress", e);
-        }
-        responseWriter.writeResponse(outputStream, jsonHelper.getObjectMapper(), httpStatusCode, responseObject);
-    }
-
-    private ProgressSummaryResponse handlePost(LambdaRequest lambdaRequest) throws Exception
-    {
-        ServiceRequest<ProgressSummaryRequest> progressSummaryRequest = new DefaultServiceRequest<>((ProgressSummaryRequest) lambdaRequest.getPayload());
-        return createProgressSummaryRequestProcessor(lambdaRequest)
-            .getProgressSummary(progressSummaryRequest);
-    }
-
-    private ProgressSummaryRequestProcessor createProgressSummaryRequestProcessor(LambdaRequest lambdaRequest)
-    {
-        return new ProgressSummaryRequestProcessor(
-            agendaProgressPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.AGENDA_PROGRESS)),
-            operationProgressPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.OPERATION_PROGRESS)),
-                agendaPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.AGENDA))
-
-        );
+        return new LambdaRequest<>(node, ProgressSummaryRequest.class);
     }
 }
