41513d77213ac6a50955825ccbde7b6a3ba77fbd
Tim Stair <tim.stair@theplatform.com>
2019-09-24 15:22:17 -0800

    [Fission] Added AgendaTemplate endpoint.
    /idm/agendatemplate is the new endpoint (can be switched to /idm/agenda/template if desired... I think)
    
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/api/": change = 539005]

---
+++ main/src/main/java/com/theplatform/dfh/cp/api/AgendaTemplate.java
@@ -0,0 +1,73 @@
+package com.theplatform.dfh.cp.api;
+
+import com.theplatform.dfh.cp.api.params.ParamsMap;
+
+public class AgendaTemplate extends EndpointDataObject
+{
+    private String title;
+    private ParamsMap templateParameters;
+    private ParamsMap staticParameters;
+    private Agenda agenda;
+    private boolean defaultTemplate;
+    private boolean globalTemplate;
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public ParamsMap getTemplateParameters()
+    {
+        return templateParameters;
+    }
+
+    public void setTemplateParameters(ParamsMap templateParameters)
+    {
+        this.templateParameters = templateParameters;
+    }
+
+    public ParamsMap getStaticParameters()
+    {
+        return staticParameters;
+    }
+
+    public void setStaticParameters(ParamsMap staticParameters)
+    {
+        this.staticParameters = staticParameters;
+    }
+
+    public Agenda getAgenda()
+    {
+        return agenda;
+    }
+
+    public void setAgenda(Agenda agenda)
+    {
+        this.agenda = agenda;
+    }
+
+    public boolean isDefaultTemplate()
+    {
+        return defaultTemplate;
+    }
+
+    public void setDefaultTemplate(boolean defaultTemplate)
+    {
+        this.defaultTemplate = defaultTemplate;
+    }
+
+    public boolean isGlobalTemplate()
+    {
+        return globalTemplate;
+    }
+
+    public void setGlobalTemplate(boolean globalTemplate)
+    {
+        this.globalTemplate = globalTemplate;
+    }
+}
