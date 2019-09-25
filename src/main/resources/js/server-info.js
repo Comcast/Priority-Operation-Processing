4ee0e368a3bdc27cdaa254a0fdeb3518fa1686e1
Tiffany Hallisey <Tiffany.Hallisey@theplatform.com>
2019-09-24 18:09:34 -0800

    babs -- Adding agenda template endpoint
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/web/": change = 539011]

--- src/main/resources/js/server-info.js
+++ src/main/resources/js/server-info.js
@@ -49,7 +49,7 @@ var endpoints =
                 path:"transform"
             },
             {
-                name:"AgendaTemplate",
+                name:"Agenda Template",
                 path:"agendatemplate"
             }
         ];
