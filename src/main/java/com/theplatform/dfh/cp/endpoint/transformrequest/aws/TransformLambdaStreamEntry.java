2eb28ebbfc70fffe1f88f2f4f9eed2dfb6ecb252
Tim Stair <tim.stair@theplatform.com>
2019-09-27 14:18:11 -0800

    [Fission] Draft of the AgendaTemplateMapper (based on old Workflow code from 2018). Add new AgendaTemplate processing in the TransformRequest endpoint (along with import/export utils)
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/impl/aws/main/": change = 539042]

--- src/main/java/com/theplatform/dfh/cp/endpoint/transformrequest/aws/TransformLambdaStreamEntry.java
+++ src/main/java/com/theplatform/dfh/cp/endpoint/transformrequest/aws/TransformLambdaStreamEntry.java
@@ -1,6 +1,7 @@
 package com.theplatform.dfh.cp.endpoint.transformrequest.aws;
 
 import com.theplatform.dfh.cp.api.Agenda;
+import com.theplatform.dfh.cp.api.AgendaTemplate;
 import com.theplatform.dfh.cp.api.TransformRequest;
 import com.theplatform.dfh.cp.api.facility.Customer;
 import com.theplatform.dfh.cp.api.facility.Insight;
@@ -8,6 +9,7 @@ import com.theplatform.dfh.cp.api.progress.AgendaProgress;
 import com.theplatform.dfh.cp.api.progress.OperationProgress;
 import com.theplatform.dfh.cp.endpoint.TableEnvironmentVariableName;
 import com.theplatform.dfh.cp.endpoint.agenda.aws.persistence.DynamoDBAgendaPersisterFactory;
+import com.theplatform.dfh.cp.endpoint.agendatemplate.aws.persistence.DynamoDBAgendaTemplatePersisterFactory;
 import com.theplatform.dfh.cp.endpoint.aws.DataObjectLambdaStreamEntry;
 import com.theplatform.dfh.cp.endpoint.aws.LambdaDataObjectRequest;
 import com.theplatform.dfh.cp.endpoint.facility.aws.persistence.DynamoDBCustomerPersisterFactory;
@@ -33,6 +35,7 @@ public class TransformLambdaStreamEntry extends DataObjectLambdaStreamEntry<Tran
     private ObjectPersisterFactory<ReadyAgenda> readyAgendaPersisterFactory;
     private ObjectPersisterFactory<Insight> insightPersisterFactory;
     private ObjectPersisterFactory<Customer> customerPersisterFactory;
+    private ObjectPersisterFactory<AgendaTemplate> agendaTemplatePersisterFactory;
 
     public TransformLambdaStreamEntry()
     {
@@ -46,6 +49,7 @@ public class TransformLambdaStreamEntry extends DataObjectLambdaStreamEntry<Tran
         readyAgendaPersisterFactory = new DynamoDbReadyAgendaPersisterFactory();
         insightPersisterFactory = new DynamoDBInsightPersisterFactory();
         customerPersisterFactory = new DynamoDBCustomerPersisterFactory();
+        agendaTemplatePersisterFactory = new DynamoDBAgendaTemplatePersisterFactory();
     }
 
     @Override
@@ -64,7 +68,8 @@ public class TransformLambdaStreamEntry extends DataObjectLambdaStreamEntry<Tran
             operationProgressPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.OPERATION_PROGRESS)),
             readyAgendaPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.READY_AGENDA)),
             insightPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.INSIGHT)),
-            customerPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.CUSTOMER))
+            customerPersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.CUSTOMER)),
+            agendaTemplatePersisterFactory.getObjectPersister(environmentLookupUtils.getTableName(lambdaRequest, TableEnvironmentVariableName.AGENDA_TEMPLATE))
         );
     }
 
