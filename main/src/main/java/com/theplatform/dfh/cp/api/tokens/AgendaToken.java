4d9ff2e825e3a57ff32d9329d44ed9cfb110f80c
Tim Stair <tim.stair@theplatform.com>
2019-10-09 10:19:40 -0800

    [Fission] Updated api to include constants for the fission tokens.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/api/": change = 539194]

---
+++ main/src/main/java/com/theplatform/dfh/cp/api/tokens/AgendaToken.java
@@ -0,0 +1,8 @@
+package com.theplatform.dfh.cp.api.tokens;
+
+public class AgendaToken
+{
+    public static final String TOKEN_PREFIX = "fission.";
+    public static final String AGENDA_ID = TOKEN_PREFIX + "agendaId";
+    public static final String OPERATION_NAME = TOKEN_PREFIX + "operationName";
+}
