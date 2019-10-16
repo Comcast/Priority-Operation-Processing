11c940341776ef902a57b6be3ebeb84820cb0546
Tim Stair <tim.stair@theplatform.com>
2019-10-16 07:40:23 -0800

    [Fission] Changed constant AgendaTokens to an enum.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/api/": change = 539312]

--- main/src/main/java/com/theplatform/dfh/cp/api/tokens/AgendaToken.java
+++ main/src/main/java/com/theplatform/dfh/cp/api/tokens/AgendaToken.java
@@ -1,8 +1,21 @@
 package com.theplatform.dfh.cp.api.tokens;
 
-public class AgendaToken
+public enum AgendaToken
 {
+    AGENDA_ID("agendaId"),
+    OPERATION_NAME("operationName")
+    ;
+
     public static final String TOKEN_PREFIX = "fission.";
-    public static final String AGENDA_ID = TOKEN_PREFIX + "agendaId";
-    public static final String OPERATION_NAME = TOKEN_PREFIX + "operationName";
+    private final String tokenSuffix;
+
+    public String getToken()
+    {
+        return TOKEN_PREFIX + tokenSuffix;
+    }
+
+    AgendaToken(String tokenSuffix)
+    {
+        this.tokenSuffix = tokenSuffix;
+    }
 }
