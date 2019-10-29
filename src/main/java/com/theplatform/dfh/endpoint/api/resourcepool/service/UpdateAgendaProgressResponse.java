e3751583be32193443aa757e43a50f5903b7ef75
Tim Stair <tim.stair@theplatform.com>
2019-10-29 12:49:37 -0800

    [Fission] Added new request/response methods for update agenda progress.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/api/main/": change = 539477]

---
+++ src/main/java/com/theplatform/dfh/endpoint/api/resourcepool/service/UpdateAgendaProgressResponse.java
@@ -0,0 +1,7 @@
+package com.theplatform.dfh.endpoint.api.resourcepool.service;
+
+import com.theplatform.dfh.endpoint.api.DefaultServiceResponse;
+
+public class UpdateAgendaProgressResponse extends DefaultServiceResponse
+{
+}
