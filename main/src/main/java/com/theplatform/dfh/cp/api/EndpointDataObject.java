284411fdaf3dc66b534edcfd6416336b4ecf7cee
Tiffany Hallisey <Tiffany.Hallisey@theplatform.com>
2019-11-01 10:11:24 -0800

    babs -- dumb p4
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/api/": change = 539562]

---
+++ main/src/main/java/com/theplatform/dfh/cp/api/EndpointDataObject.java
@@ -0,0 +1,17 @@
+package com.theplatform.dfh.cp.api;
+
+import java.util.Date;
+
+public interface EndpointDataObject
+{
+    public String getId();
+
+    public String getCustomerId();
+
+    public String getCid();
+
+    public Date getUpdatedTime();
+
+    public Date getAddedTime();
+
+}
