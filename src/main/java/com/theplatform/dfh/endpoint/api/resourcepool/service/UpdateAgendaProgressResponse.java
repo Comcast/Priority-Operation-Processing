79fb691f4922856eac4ed3404e265354e61e64c5
Tim Stair <tim.stair@theplatform.com>
2019-11-07 12:14:45 -0800

    [Fission] Start to form the necessary changes to support updateAgendaProgress visibility.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/api/main/": change = 539627]

--- src/main/java/com/theplatform/dfh/endpoint/api/resourcepool/service/UpdateAgendaProgressResponse.java
+++ src/main/java/com/theplatform/dfh/endpoint/api/resourcepool/service/UpdateAgendaProgressResponse.java
@@ -1,7 +1,14 @@
 package com.theplatform.dfh.endpoint.api.resourcepool.service;
 
 import com.theplatform.dfh.endpoint.api.DefaultServiceResponse;
+import com.theplatform.dfh.endpoint.api.ErrorResponse;
 
 public class UpdateAgendaProgressResponse extends DefaultServiceResponse
 {
+    public UpdateAgendaProgressResponse(){}
+
+    public UpdateAgendaProgressResponse(ErrorResponse errorResponse)
+    {
+        setErrorResponse(errorResponse);
+    }
 }
