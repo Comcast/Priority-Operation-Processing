b1c0ed11850439288337eeedd8e15b21d207cca8
Tim Stair <tim.stair@comcast.com>
2020-01-16 15:02:49 -0800

    Added a progress message with a param.

--- impl/src/main/java/com/theplatform/dfh/cp/handler/sample/impl/action/BaseAction.java
+++ impl/src/main/java/com/theplatform/dfh/cp/handler/sample/impl/action/BaseAction.java
@@ -1,15 +1,21 @@
 package com.theplatform.dfh.cp.handler.sample.impl.action;
 
+import com.theplatform.dfh.cp.api.params.ParamsMap;
+import com.theplatform.dfh.cp.api.progress.OperationProgress;
+import com.theplatform.dfh.cp.api.progress.ProcessingState;
 import com.theplatform.dfh.cp.handler.base.progress.reporter.operation.OperationProgressReporter;
 import com.theplatform.dfh.cp.handler.sample.api.ActionParameters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.UUID;
 import java.util.stream.IntStream;
 
 public abstract class BaseAction
 {
     private static Logger logger = LoggerFactory.getLogger(BaseAction.class);
+
+    private static final String JOB_ID = UUID.randomUUID().toString();
 
     public void performAction(OperationProgressReporter reporter, ActionParameters actionParameters)
     {
@@ -21,7 +27,7 @@ public abstract class BaseAction
             final int ITERATIONS = Math.max(1, (int)(sleepMilliseconds/1000));
             IntStream.range(0, ITERATIONS).forEach(i ->
                 {
-                    reporter.addProgress(((double)i/(double)ITERATIONS) * 100d);
+                    sendProgress(reporter, ((double)i/(double)ITERATIONS) * 100d);
                     try
                     {
                         Thread.sleep(1000);
@@ -30,11 +36,23 @@ public abstract class BaseAction
                     {
                         throw new RuntimeException("Thread interrupted!", e);
                     }
-
                 }
             );
         }
         perform(reporter, actionParameters);
+    }
+
+    protected void sendProgress(OperationProgressReporter reporter, Double progress)
+    {
+        ParamsMap paramsMap = new ParamsMap();
+        paramsMap.put("JobId", JOB_ID);
+
+        OperationProgress operationProgress = new OperationProgress();
+        operationProgress.setParams(paramsMap);
+        operationProgress.setProcessingState(ProcessingState.EXECUTING);
+        operationProgress.setPercentComplete(progress);
+
+        reporter.addOperationProgress(operationProgress);
     }
 
     protected abstract void perform(OperationProgressReporter reporter, ActionParameters actionParameters);
