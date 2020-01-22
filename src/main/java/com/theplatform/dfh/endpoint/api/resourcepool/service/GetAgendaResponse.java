84c2767e036666e2ade1e5634a78551baf75bbdc
Tim Stair <tim.stair@theplatform.com>
2020-01-22 12:18:13 -0800

    [Fission] Changes to the GetAgendaResponse to support continue-from-point-of-failure.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/api/main/": change = 540549]

--- src/main/java/com/theplatform/dfh/endpoint/api/resourcepool/service/GetAgendaResponse.java
+++ src/main/java/com/theplatform/dfh/endpoint/api/resourcepool/service/GetAgendaResponse.java
@@ -1,13 +1,21 @@
 package com.theplatform.dfh.endpoint.api.resourcepool.service;
 
 import com.theplatform.dfh.cp.api.Agenda;
+import com.theplatform.dfh.cp.api.progress.AgendaProgress;
 import com.theplatform.dfh.endpoint.api.DataObjectFeedServiceResponse;
 import com.theplatform.dfh.endpoint.api.ErrorResponse;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class GetAgendaResponse extends DataObjectFeedServiceResponse<Agenda>
 {
+    private List<AgendaProgress> agendaProgresses;
+
     public GetAgendaResponse()
     {
     }
@@ -22,6 +30,12 @@ public class GetAgendaResponse extends DataObjectFeedServiceResponse<Agenda>
         super(dataObjects);
     }
 
+    public GetAgendaResponse(Collection<Agenda> dataObjects, List<AgendaProgress> agendaProgresses)
+    {
+        super(dataObjects);
+        this.agendaProgresses = agendaProgresses;
+    }
+
     public void setAgendas(Collection<Agenda> dataObjects)
     {
         super.setAll(dataObjects);
@@ -30,5 +44,30 @@ public class GetAgendaResponse extends DataObjectFeedServiceResponse<Agenda>
     public Collection<Agenda> getAgendas()
     {
         return super.getAll();
+    }
+
+    public List<AgendaProgress> getAgendaProgresses()
+    {
+        return agendaProgresses;
+    }
+
+    public void setAgendaProgresses(List<AgendaProgress> agendaProgresses)
+    {
+        this.agendaProgresses = agendaProgresses;
+    }
+
+    /**
+     * Retrieves a map of Agenda to AgendaProgress objects based on the response. The AgendaProgress may be null.
+     * @return Map of Agenda to AgendaProgress objects
+     */
+    public Map<Agenda, AgendaProgress> retrieveAgendaToProgressMap()
+    {
+        Map<String, AgendaProgress> agendaProgressMap = getAgendaProgresses() == null
+            ? new HashMap<>()
+            : getAgendaProgresses().stream().collect(Collectors.toMap(AgendaProgress::getId, Function.identity()));
+
+        return getAgendas() == null
+            ? new HashMap<>()
+            : getAgendas().stream().collect(Collectors.toMap(Function.identity(), agenda -> agendaProgressMap.get(agenda.getProgressId())));
     }
 }
