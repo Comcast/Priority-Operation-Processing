77bd71a13632da6b05a3980cd13c1c473a809186
Tim Stair <tim.stair@theplatform.com>
2020-01-21 16:03:54 -0800

    [Fission] Added status and conclusion message to the op complete log.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/handler/executor/": change = 540539]

--- handler/main/impl/src/main/java/com/theplatform/dfh/cp/handler/executor/impl/processor/runner/event/OperationCompleteLog.java
+++ handler/main/impl/src/main/java/com/theplatform/dfh/cp/handler/executor/impl/processor/runner/event/OperationCompleteLog.java
@@ -1,5 +1,6 @@
 package com.theplatform.dfh.cp.handler.executor.impl.processor.runner.event;
 
+import com.sun.tools.javac.util.StringUtils;
 import com.theplatform.dfh.cp.api.operation.Operation;
 import com.theplatform.dfh.cp.api.progress.OperationProgress;
 import com.theplatform.dfh.cp.handler.executor.impl.context.ExecutorContext;
@@ -31,13 +32,15 @@ public class OperationCompleteLog implements OperationCompleteEvent
 
         logger.info(
             "Completed operation " +
-            "agendaId={} externalId={} agendaProgressId={} operationType={} operationName={} operationProgressId={} runTime={}",
+            "agendaId={} externalId={} agendaProgressId={} operationType={} operationName={} operationProgressId={} state={} conclusion={} runTime={}",
             executorContext.getAgendaId(),
             executorContext.getAgenda().getLinkId(),
             executorContext.getAgendaProgressId(),
             operation.getType(),
             operation.getName(),
             operationProgressId,
+            StringUtils.toLowerCase(operationProgress.getProcessingState().name()),
+            operationProgress.getProcessingStateMessage(),
             runTimeMs
         );
     }
