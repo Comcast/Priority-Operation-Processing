e0a0d3b15f3f1f82bbdeafd6cfeadaad1299a68b
Chris Orogvany <chris.orogvany@theplatform.com>
2020-02-26 16:34:47 -0800

    Update Transform/Task to use IgniteAgenda for consistency
    Also some minor dependency updates.
    
    This does require that we look up the AgendaTemplate twice, as the `hack` to create execProgress needs the template.  This will eventually go away which should simplify the logic.
    
    Started to stub the isResubmit functionality, but some todos.  It should be still in functionally the same state as it was before, so we can verify working, then move onto implementing the resubmit logic
    
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/api/main/": change = 540902]

--- src/main/java/com/theplatform/dfh/endpoint/api/agenda/service/IgniteAgendaResponse.java
+++ src/main/java/com/theplatform/dfh/endpoint/api/agenda/service/IgniteAgendaResponse.java
@@ -1,6 +1,9 @@
 package com.theplatform.dfh.endpoint.api.agenda.service;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.theplatform.dfh.cp.api.Agenda;
+import com.theplatform.dfh.cp.api.AgendaTemplate;
+import com.theplatform.dfh.cp.api.TransformRequest;
 import com.theplatform.dfh.endpoint.api.DataObjectFeedServiceResponse;
 import com.theplatform.dfh.endpoint.api.ErrorResponse;
 
@@ -30,6 +33,12 @@ public class IgniteAgendaResponse extends DataObjectFeedServiceResponse<Agenda>
     public Collection<Agenda> getAgendas()
     {
         return super.getAll();
+    }
+
+    @JsonIgnore
+    public Agenda getFirst()
+    {
+        return super.getAll().iterator().next();
     }
 }
 
