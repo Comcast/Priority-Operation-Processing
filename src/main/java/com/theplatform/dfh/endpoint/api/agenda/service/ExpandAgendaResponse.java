83418ea876746f1f7f37e925395f2b863e42a164
Tim Stair <tim.stair@theplatform.com>
2020-03-06 15:04:34 -0800

    [DFH-4686] Expand agenda function (name may change)
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/api/main/": change = 540995]

---
+++ src/main/java/com/theplatform/dfh/endpoint/api/agenda/service/ExpandAgendaResponse.java
@@ -0,0 +1,11 @@
+package com.theplatform.dfh.endpoint.api.agenda.service;
+
+import com.theplatform.dfh.endpoint.api.DefaultServiceResponse;
+
+public class ExpandAgendaResponse extends DefaultServiceResponse
+{
+    public ExpandAgendaResponse()
+    {
+    }
+}
+
