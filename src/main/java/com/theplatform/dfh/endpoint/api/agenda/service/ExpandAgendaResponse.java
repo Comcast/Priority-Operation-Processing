d6f7a8e4553f0276a7d3e78709faf8a1e8c91405
Tim Stair <tim.stair@theplatform.com>
2020-03-09 07:25:38 -0800

    [Fission] Additional unit test coverage on the ExpandAgenda call.
    [git-p4: depot-paths = "//depot/service/java/projects/DistributedFileHandling/ContentProcessing/endpoint/api/main/": change = 541002]

--- src/main/java/com/theplatform/dfh/endpoint/api/agenda/service/ExpandAgendaResponse.java
+++ src/main/java/com/theplatform/dfh/endpoint/api/agenda/service/ExpandAgendaResponse.java
@@ -1,11 +1,24 @@
 package com.theplatform.dfh.endpoint.api.agenda.service;
 
+import com.theplatform.dfh.cp.api.Agenda;
 import com.theplatform.dfh.endpoint.api.DefaultServiceResponse;
 
 public class ExpandAgendaResponse extends DefaultServiceResponse
 {
+    private Agenda agenda;
+
     public ExpandAgendaResponse()
     {
+    }
+
+    public Agenda getAgenda()
+    {
+        return agenda;
+    }
+
+    public void setAgenda(Agenda agenda)
+    {
+        this.agenda = agenda;
     }
 }
 
