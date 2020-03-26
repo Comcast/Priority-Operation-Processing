d31b97ed4d6318ecabe9748d900eaea9f15e52a6
corogv000 <chris.orogvany@comcast.com>
2020-03-26 10:33:06 -0700

    Update for more dependency pruning

--- migrated/module/module-handler-kubernetes-support/src/main/java/com/theplatform/dfh/cp/handler/kubernetes/support/monitor/AliveCheckPollerFactory.java
+++ migrated/module/module-handler-kubernetes-support/src/main/java/com/theplatform/dfh/cp/handler/kubernetes/support/monitor/AliveCheckPollerFactory.java
@@ -5,8 +5,6 @@ import com.theplatform.dfh.cp.modules.monitor.alert.AlertConfigKeys;
 import com.theplatform.dfh.cp.modules.monitor.alive.AliveCheck;
 import com.theplatform.dfh.cp.modules.monitor.alive.AliveCheckConfigKeys;
 import com.theplatform.dfh.cp.modules.monitor.alive.AliveCheckPoller;
-import com.theplatform.dfh.cp.modules.monitor.bananas.BananasAliveCheckListener;
-import com.theplatform.dfh.cp.modules.monitor.bananas.config.BananasConfigKeys;
 import com.theplatform.dfh.cp.modules.monitor.config.ConfigurationProperties;
 import com.theplatform.dfh.cp.modules.monitor.metric.MetricAliveCheckListener;
 import com.theplatform.dfh.cp.modules.monitor.metric.MetricReporter;
@@ -29,11 +27,10 @@ public class AliveCheckPollerFactory
             {
                 Properties properties = propertyRetriever.getPropertyProvider().getProperties();
                 ConfigurationProperties configuationProperties =
-                    ConfigurationProperties.from(properties, new BananasConfigKeys(), new AlertConfigKeys(), new AliveCheckConfigKeys());
-                BananasAliveCheckListener bananasAliveCheck = new BananasAliveCheckListener(configuationProperties);
+                    ConfigurationProperties.from(properties, new AlertConfigKeys(), new AliveCheckConfigKeys());
                 MetricAliveCheckListener metricAliveCheckListener = new MetricAliveCheckListener(metricReporter);
                 //start alive check poller.
-                AliveCheckPoller poller = new AliveCheckPoller(configuationProperties, aliveCheck, Arrays.asList(bananasAliveCheck, metricAliveCheckListener));
+                AliveCheckPoller poller = new AliveCheckPoller(configuationProperties, aliveCheck, Arrays.asList( metricAliveCheckListener));
                 poller.start();
                 return poller;
             }
@@ -41,7 +38,7 @@ public class AliveCheckPollerFactory
         catch (Throwable e)
         {
             //don't crash our application if we can't send alerts.
-            logger.error("Error occurred trying to initialize alive check via Bananas. Alive check is now disabled.", e);
+            logger.error("Error occurred trying to initialize alive check. Alive check is now disabled.", e);
         }
         return null;
     }
